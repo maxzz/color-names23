2710e10e888a3d71ce5f62e14340d9339d2de96e
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-09 14:20:31 -0700

    WIP. header. fix links decorations

--- src/components/app/3-footer.tsx
+++ src/components/app/3-footer.tsx
@@ -1,62 +1,54 @@
-import React, { HTMLAttributes } from 'react';
+import { AnchorHTMLAttributes, HTMLAttributes } from 'react';
 import { useAtomValue } from 'jotai';
 import { AppAtoms, SectionName } from '@/store';
 import { IconGithubLogo } from '@/components/ui/icons/normal';
 import { classNames } from '@/utils';
 import { ThemeSwitch } from '../ui/shadcn/theme-toggle-switch';
 
+function LinkButton(props: AnchorHTMLAttributes<HTMLAnchorElement>) {
+    return (
+        <a className="hover:text-foreground underline decoration-[0.04rem] underline-offset-[0.2rem] decoration-dotted decoration-muted-foreground/50 hover:decoration-foreground" target="_blank" {...props} />
+    );
+}
+
 function Section1_HueLinks() {
     return (<>
-        <a className="hover:text-primary-400 hover:underline" href="https://enes.in/sorted-colors" target="_blank" title="Mustafa Enes sorted-colors">
-            original
-        </a>
-
-        <a className="hover:text-primary-400 hover:underline" href="https://maxzz.github.io/color-names" target="_blank" title="My sorted-colors take one">
-            prev
-        </a>
+        <LinkButton href="https://enes.in/sorted-colors" title="Mustafa Enes sorted-colors">Original project</LinkButton>
+        <LinkButton href="https://maxzz.github.io/color-names" title="My sorted-colors take one">My initial project</LinkButton>
     </>);
 }
 
 function Section2_ColorListLinks() {
     return (<>
-        <a className="hover:text-primary-400 hover:underline" href="https://meyerweb.com/eric/css/colors" target="_blank" title="Color equivalents table">
-            meyerweb
-        </a>
+        <LinkButton href="https://meyerweb.com/eric/css/colors">Color equivalents table (meyerweb.com)</LinkButton>
     </>);
 }
 
 function Section3_TailwindLinks() {
     return (<>
-        <a className="hover:text-primary-400 hover:underline" href="https://tailwindcss.com/docs/customizing-colors" target="_blank" title="Colors on Tailwind CSS website">
-            Tailwind CSS palettes
-        </a>
+        <LinkButton href="https://tailwindcss.com/docs/customizing-colors" title="Colors on Tailwind CSS website">Tailwind CSS palettes</LinkButton>
     </>);
 }
 
 function GitHubLink() {
     return (
-        <a href="https://github.com/maxzz/color-names22" target="_blank">
-            <IconGithubLogo
-                className="w-5 h-5 fill-primary-200 stroke-[50] stroke-current hover:stroke-primary-200 hover:scale-[1.5] hover:stroke-[34] transition-all"
-                title="Open the source code of the project on Github"
-            />
-        </a>
+        <LinkButton href="https://github.com/maxzz/color-names23" title="Open the project's source code on Github">
+            <IconGithubLogo className="w-5 h-5 fill-primary-200 stroke-current hover:stroke-foreground stroke-[50] hover:stroke-[34] hover:scale-[1.5] transition-all" />
+        </LinkButton>
     );
 }
 
 export function App3_Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    const currentSection = useAtomValue(AppAtoms.currentSectionAtom);
+    const current = useAtomValue(AppAtoms.currentSectionAtom);
     return (
-        <div className={classNames(
-            "px-2 py-2 text-sm flex items-center justify-between space-x-4 bg-title text-primary-400/75",
-            //"border-primary-400 border-t shadow-[0px_-1px_2px_1px_#b69a7950]",
-            className,
-        )} {...rest}>
+        <div className={classNames("px-2 py-2 text-sm text-muted-foreground bg-background flex items-center justify-between space-x-4", className)} {...rest}>
+
             <div className="flex items-center space-x-4">
                 <GitHubLink />
-                {currentSection === SectionName.hue && <Section1_HueLinks />}
-                {currentSection === SectionName.list && <Section2_ColorListLinks />}
-                {currentSection === SectionName.tailwind && <Section3_TailwindLinks />}
+
+                {current === SectionName.hue && <Section1_HueLinks />}
+                {current === SectionName.list && <Section2_ColorListLinks />}
+                {current === SectionName.tailwind && <Section3_TailwindLinks />}
             </div>
 
             <ThemeSwitch />
