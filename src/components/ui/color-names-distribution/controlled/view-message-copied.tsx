543a7c6b330544509ca3b9a3c7ddad11a9ffc370
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-13 15:38:46 -0700

    WIP. hueWheel. MessageHueColorCopied. add copied color

--- src/components/ui/color-names-distribution/controlled/view-message-copied.tsx
+++ src/components/ui/color-names-distribution/controlled/view-message-copied.tsx
@@ -9,12 +9,13 @@ export function MessageHueColorCopied({ className, ...rest }: HTMLAttributes<HTM
     const styles = useSpring({
         opacity: text ? 1 : 0,
         transform: text ? 'translateY(0)' : 'translateY(100%)',
-        config: { easing: easings.easeOutElastic, duration: 1500 },
+        config: { easing: easings.easeOutElastic, duration: 31500 },
         onRest: () => text && (colorToCopyState.text = ''),
     });
     return (
-        <a.div style={styles} className={classNames("px-2 py-1 text-xs text-white bg-green-600 rounded", className)} {...rest}>
-            Copied
+        <a.div style={styles} className={classNames("px-2 py-1 text-xs text-white bg-green-600 rounded flex items-center space-x-2", className)} {...rest}>
+            <div className="w-4 h-4 border-green-950 border-2" style={{backgroundColor: text}}></div>
+            <div>Copied</div>
         </a.div>
     );
 }
