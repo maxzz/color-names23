60ab2cb20fff278e508f4dc2dc8b0c29e8d88506
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-13 15:50:13 -0700

    WIP. hueWheel. MessageHueColorCopied. fix exit animation

--- src/components/ui/color-names-distribution/controlled/view-message-copied.tsx
+++ src/components/ui/color-names-distribution/controlled/view-message-copied.tsx
@@ -9,12 +9,12 @@ export function MessageHueColorCopied({ className, ...rest }: HTMLAttributes<HTM
     const styles = useSpring({
         opacity: text ? 1 : 0,
         transform: text ? 'translateY(0)' : 'translateY(100%)',
-        config: { easing: easings.easeOutElastic, duration: 31500 },
+        config: { easing: text ? easings.easeOutElastic : easings.linear, duration: text ? 1000 : 1400 },
         onRest: () => text && (colorToCopyState.text = ''),
     });
     return (
         <a.div style={styles} className={classNames("px-2 py-1 text-xs text-white bg-green-600 rounded flex items-center space-x-2", className)} {...rest}>
-            <div className="w-4 h-4 border-green-950 border-2" style={{backgroundColor: text}}></div>
+            <div className="w-4 h-4 border-green-800 border rounded" style={{backgroundColor: text}}></div>
             <div>Copied</div>
         </a.div>
     );
