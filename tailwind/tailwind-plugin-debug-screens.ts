4269c678749a422df8437c0130ffefe1877c537a
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-14 14:14:12 -0700

    WIP. tailwind. debug-screens. code clean up

--- tailwind/tailwind-plugin-debug-screens.ts
+++ tailwind/tailwind-plugin-debug-screens.ts
@@ -5,7 +5,7 @@ module.exports = plugin(
     function ({ addComponents, theme }) {
         //https://github.com/jorenvanhee/tailwindcss-debug-screens // use: add class 'debug-screens' on any top element
 
-        const screens = theme('screens')!; // screens { xs: '420px', sm: '640px', md: '768px', lg: '1024px', xl: '1280px' /*or xl: '1350px'*/, '2xl': '1536px', '3xl': '1920px'; }
+        const screens = theme('screens'); // screens { xs: '420px', sm: '640px', md: '768px', lg: '1024px', xl: '1280px' /*or xl: '1350px'*/, '2xl': '1536px', '3xl': '1920px'; }
         const userStyles = theme('debugScreens.style', {});
         const ignoredScreens = theme('debugScreens.ignore', ['dark']);
         const prefix = theme('debugScreens.prefix', 'screen: ');
@@ -16,11 +16,11 @@ module.exports = plugin(
         const positionX = position[1] || defaultPosition[1];
         const positionY = position[0] || defaultPosition[0];
 
-        //const screenEntries = Object.entries(screens);
         const screenEntries = sortScreenEntries(screens);
-
         const lowestScreenEntryName = screenEntries?.[0]?.[0];
         const lowestScreenEntrSize = screenEntries?.[0]?.[1];
+
+        //console.log('----------------------- screenEntries', JSON.stringify(screenEntries));
 
         const components: any = {
             [`${selector}::before`]:
@@ -56,255 +56,73 @@ module.exports = plugin(
     },
     {
         theme: {
-            // debugScreens: {
             screens: {
                 'xs': '501px', // Chrome minimum screen size is 500px
                 ...twTheme.screens,
             }
-            // }
         }
     }
 );
 
-//console.log('----------------------- screenEntries', screenEntries);
-// console.log('----------------------- screens', screens);
-// console.log('----------------------- normalizeScreens', JSON.stringify(normalizeScreens(screens), null, 4));
-//console.log('----------------------- extractMinWidths', JSON.stringify(extractMinWidths(normalizeScreens(screens)), null, 4));
-// console.log('----------------------- extractMinWidths2', JSON.stringify(Object.fromEntries(extractMinWidths2(normalizeScreens(screens))), null, 4));
-//console.log('----------------------- sortScreenEntries', JSON.stringify(Object.fromEntries(sortScreenEntries(screens)), null, 4));
-//console.log('----------------------- screenEntries', JSON.stringify(screenEntries), null, 4);
-
 function sortScreenEntries(screens) {
     const normalized = normalizeScreens(screens);
-    const newScreens = extractMinWidths2(normalized);
-    //newScreens.push(['xsm', '501px']); // This smallest screen allowed by Chrome.
-    //newScreens.push(['xs', '501px']); // This smallest screen allowed by Chrome.
+    const newScreens = extractScreenValues(normalized);
     newScreens.sort((a, b) => parseInt(a[1]) - parseInt(b[1]));
-    console.log('----------------------- newScreens', JSON.stringify(newScreens, null, 4));
     return newScreens;
-}
 
-function resolveValue({ 'min-width': _minWidth, min = _minWidth, max, raw }) { // it was: resolveValue({ 'min-width': _minWidth, min = _minWidth, max, raw } = {}) {
-    return { min, max, raw };
-}
-
-function normalizeScreens(screens, root = true): { name: any; not: boolean; values: any[]; }[] {
-    if (Array.isArray(screens)) {
-        return screens.map((screen) => {
-            if (root && Array.isArray(screen)) {
-                throw new Error('The tuple syntax is not supported for `screens`.');
-            }
-
-            if (typeof screen === 'string') {
-                return { name: screen.toString(), not: false, values: [{ min: screen, max: undefined }] };
-            }
-
-            let [name, options] = screen;
-            name = name.toString();
-
-            if (typeof options === 'string') {
-                return { name, not: false, values: [{ min: options, max: undefined }] };
-            }
-
-            if (Array.isArray(options)) {
-                return { name, not: false, values: options.map((option) => resolveValue(option)) };
-            }
-
-            return { name, not: false, values: [resolveValue(options)] };
-        });
+    type NormalizeScreenValue = {
+        min: string;
+        max: string | undefined;
+        raw?: string;
+    };
+    
+    type NormalizeScreen = {
+        name: string;
+        not: boolean;
+        values: NormalizeScreenValue[];
+    };
+    
+    function normalizeScreens(screens, root = true): NormalizeScreen[] {
+        if (Array.isArray(screens)) {
+            return screens.map((screen) => {
+                if (root && Array.isArray(screen)) {
+                    throw new Error('The tuple syntax is not supported for `screens`.');
+                }
+    
+                if (typeof screen === 'string') {
+                    return { name: screen.toString(), not: false, values: [{ min: screen, max: undefined }] };
+                }
+    
+                let [name, options] = screen;
+                name = name.toString();
+    
+                if (typeof options === 'string') {
+                    return { name, not: false, values: [{ min: options, max: undefined }] };
+                }
+    
+                if (Array.isArray(options)) {
+                    return { name, not: false, values: options.map((option) => resolveValue(option)) };
+                }
+    
+                return { name, not: false, values: [resolveValue(options)] };
+            });
+        }
+    
+        return normalizeScreens(Object.entries(screens ?? {}), false);
+    
+        function resolveValue({ 'min-width': _minWidth, min = _minWidth, max, raw }): NormalizeScreenValue {
+            return { min, max, raw }; // it was: resolveValue({ 'min-width': _minWidth, min = _minWidth, max, raw } = {}) {
+        }
     }
-
-    return normalizeScreens(Object.entries(screens ?? {}), false);
-}
-
-function extractMinWidths(breakpoints: any[] = []) {
-    return breakpoints
-        .flatMap((breakpoint) => breakpoint.values.map((breakpoint) => breakpoint.min))
-        .filter((v) => v !== undefined);
-}
-
-function extractMinWidths2(breakpoints: any[] = []) {
-    return breakpoints
-        .flatMap((breakpoint) => breakpoint.values.map((brk) => [breakpoint.name, brk.min]))
-        .filter((v) => v !== undefined);
+    
+    function extractScreenValues(breakpoints: NormalizeScreen[] = []): string[][] {
+        return breakpoints
+            .flatMap((breakpoint) => breakpoint.values.map((brk) => [breakpoint.name, brk.min]))
+            .filter((v) => v !== undefined);
+    }
 }
 
 /*
-console.log('----------------------- extractMinWidths2', JSON.stringify(extractMinWidths2(normalizeScreens(screens)), null, 4));
-
-function extractMinWidths2(breakpoints = []) {
-    return breakpoints
-      .flatMap((breakpoint) => breakpoint.values.map((brk) => [breakpoint.name, brk.min]))
-      .filter((v) => v !== undefined)
-}
-
------------------------ extractMinWidths2 [
-    [
-        "sm",
-        "640px"
-    ],
-    [
-        "md",
-        "768px"
-    ],
-    [
-        "lg",
-        "1024px"
-    ],
-    [
-        "xl",
-        "1350px"
-    ],
-    [
-        "2xl",
-        "1536px"
-    ],
-    [
-        "xs",
-        "420px"
-    ],
-    [
-        "3xl",
-        "1920px"
-    ]
-]
-*/
-
-/*
------------------------ screenEntries [
-[ 'sm', '640px' ],
-[ 'md', '768px' ],
-[ 'lg', '1024px' ],
-[ 'xl', '1350px' ],
-[ '2xl', '1536px' ],
-[ 'xs', '420px' ],
-[ '3xl', '1920px' ]
-]
------------------------ screens {
-sm: '640px',
-md: '768px',
-lg: '1024px',
-xl: '1350px',
-'2xl': '1536px',
-xs: '420px',
-'3xl': '1920px'
-}
------------------------ normalizeScreens [
-  {
-      "name": "sm",
-      "not": false,
-      "values": [
-          {
-              "min": "640px"
-          }
-      ]
-  },
-  {
-      "name": "md",
-      "not": false,
-      "values": [
-          {
-              "min": "768px"
-          }
-      ]
-  },
-  {
-      "name": "lg",
-      "not": false,
-      "values": [
-          {
-              "min": "1024px"
-          }
-      ]
-  },
-  {
-      "name": "xl",
-      "not": false,
-      "values": [
-          {
-              "min": "1350px"
-          }
-      ]
-  },
-  {
-      "name": "2xl",
-      "not": false,
-      "values": [
-          {
-              "min": "1536px"
-          }
-      ]
-  },
-  {
-      "name": "xs",
-      "not": false,
-      "values": [
-          {
-              "min": "420px"
-          }
-      ]
-  },
-  {
-      "name": "3xl",
-      "not": false,
-      "values": [
-          {
-              "min": "1920px"
-          }
-      ]
-  }
-]
------------------------ extractMinWidths [
-  "640px",
-  "768px",
-  "1024px",
-  "1350px",
-  "1536px",
-  "420px",
-  "1920px"
-]
-*/
-
-/*
-// ------------------------------------- This is the problem in the generated css (search for 2147483647):
-
-@media (min-width: 640px) {
-
-  .debug-screens::before {
-    content: 'screen: sm (640px)';
-  }
-}
-@media (min-width: 768px) {
-
-  .debug-screens::before {
-    content: 'screen: md (768px)';
-  }
-}
-@media (min-width: 1024px) {
-
-  .debug-screens::before {
-    content: 'screen: lg (1024px)';
-  }
-}
-@media (min-width: 1280px) {
-
-  .debug-screens::before {
-    content: 'screen: xl (1280px)';
-  }
-}
-@media (min-width: 1536px) {
-
-  .debug-screens::before {
-    content: 'screen: 2xl (1536px)';
-  }
-}
-@media (min-width: 501px) {
-
-  .debug-screens::before {
-    content: 'screen: smallest (501px)';
-  }
-}
-
 // ------------------------------------- This is how it should be:
 
   .debug-screens::before{
@@ -378,5 +196,5 @@ xs: '420px',
                 '3xl': '1920px',
             },
 
-// ------------------------------------- custome screens from tailwind.config.js should override the default ones.
+// ------------------------------------- custom screens from tailwind.config.js should override the default ones.
 */
