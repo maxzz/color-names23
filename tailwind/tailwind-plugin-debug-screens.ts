c0db8a75be1cfd116c31b993e866b46a24656349
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-13 20:09:09 -0700

    WIP. tailwind. debug-screens. still error: No `sm0` screen found.

--- tailwind/tailwind-plugin-debug-screens.ts
+++ tailwind/tailwind-plugin-debug-screens.ts
@@ -1,77 +1,88 @@
 import plugin from 'tailwindcss/plugin';
 
-module.exports = plugin(function ({ addComponents, theme }) {
-    //https://github.com/jorenvanhee/tailwindcss-debug-screens
-    //use: add class 'debug-screens' on any top element
+module.exports = plugin(
+    function ({ addComponents, theme }) {
+        //https://github.com/jorenvanhee/tailwindcss-debug-screens
+        //use: add class 'debug-screens' on any top element
 
-    const screens = theme('screens')!; // {sm: '640px', md: '768px', lg: '1024px', xl: '1280px', '2xl': '1536px'}
+        const screens = theme('screens')!; // {sm: '640px', md: '768px', lg: '1024px', xl: '1280px', '2xl': '1536px'}
 
-    const userStyles = theme('debugScreens.style', {});
-    const ignoredScreens = theme('debugScreens.ignore', ['dark']);
-    const prefix = theme('debugScreens.prefix', 'screen: ');
-    const selector = theme('debugScreens.selector', '.debug-screens');
+        const userStyles = theme('debugScreens.style', {});
+        const ignoredScreens = theme('debugScreens.ignore', ['dark']);
+        const prefix = theme('debugScreens.prefix', 'screen: ');
+        const selector = theme('debugScreens.selector', '.debug-screens');
 
-    const defaultPosition = ['bottom', 'left'];
-    const position = theme('debugScreens.position', defaultPosition);
-    const positionY = position[0] || defaultPosition[0];
-    const positionX = position[1] || defaultPosition[1];
+        const defaultPosition = ['bottom', 'left'];
+        const position = theme('debugScreens.position', defaultPosition);
+        const positionY = position[0] || defaultPosition[0];
+        const positionX = position[1] || defaultPosition[1];
 
-    const screenEntries = Object.entries(screens);
-    //const screenEntries = sortScreenEntries(screens);
+        //const screenEntries = Object.entries(screens);
+        const screenEntries = sortScreenEntries(screens);
 
-    const components: any = {
-        [`${selector}::before`]: Object.assign({
-            position: 'fixed',
-            zIndex: '2147483647',
-            [positionY]: '6px',
-            [positionX]: '4px',
-            padding: '.5em',
-            fontSize: '12px',
-            lineHeight: '1',
-            fontFamily: 'sans-serif',
-            borderRadius: '3px',
-            border: '1px solid #b1b1b1',
-            backgroundColor: '#0008',
-            color: '#ddd',
-            boxShadow: '0 0 2px 2px #fff5',
-            content: `'${prefix}${screenEntries?.[0]?.[0] ? `less then ${screenEntries?.[0]?.[0]} (${screenEntries?.[0]?.[1]})` : '_'}'`,
-        }, userStyles),
-    };
+        const components: any = {
+            [`${selector}::before`]: Object.assign({
+                position: 'fixed',
+                zIndex: '2147483647',
+                [positionY]: '6px',
+                [positionX]: '4px',
+                padding: '.5em',
+                fontSize: '12px',
+                lineHeight: '1',
+                fontFamily: 'sans-serif',
+                borderRadius: '3px',
+                border: '1px solid #b1b1b1',
+                backgroundColor: '#0008',
+                color: '#ddd',
+                boxShadow: '0 0 2px 2px #fff5',
+                content: `'${prefix}${screenEntries?.[0]?.[0] ? `less then ${screenEntries?.[0]?.[0]} (${screenEntries?.[0]?.[1]})` : '_'}'`,
+            }, userStyles),
+        };
 
-    console.log('----------------------- screenEntries', screenEntries);
-    // console.log('----------------------- screens', screens);
-    // console.log('----------------------- normalizeScreens', JSON.stringify(normalizeScreens(screens), null, 4));
-    //console.log('----------------------- extractMinWidths', JSON.stringify(extractMinWidths(normalizeScreens(screens)), null, 4));
-    // console.log('----------------------- extractMinWidths2', JSON.stringify(Object.fromEntries(extractMinWidths2(normalizeScreens(screens))), null, 4));
-    //console.log('----------------------- sortScreenEntries', JSON.stringify(Object.fromEntries(sortScreenEntries(screens)), null, 4));
-    //console.log('----------------------- screenEntries', JSON.stringify(screenEntries), null, 4);
+        //console.log('----------------------- screenEntries', screenEntries);
+        // console.log('----------------------- screens', screens);
+        // console.log('----------------------- normalizeScreens', JSON.stringify(normalizeScreens(screens), null, 4));
+        //console.log('----------------------- extractMinWidths', JSON.stringify(extractMinWidths(normalizeScreens(screens)), null, 4));
+        // console.log('----------------------- extractMinWidths2', JSON.stringify(Object.fromEntries(extractMinWidths2(normalizeScreens(screens))), null, 4));
+        //console.log('----------------------- sortScreenEntries', JSON.stringify(Object.fromEntries(sortScreenEntries(screens)), null, 4));
+        //console.log('----------------------- screenEntries', JSON.stringify(screenEntries), null, 4);
 
-    function sortScreenEntries(screens) {
-        const normalized = normalizeScreens(screens);
-        const newScreens = extractMinWidths2(normalized);
-        //newScreens.push(['xsm', '501px']); // This smallest screen allowed by Chrome.
-        newScreens.push(['sm0', '501px']); // This smallest screen allowed by Chrome.
-        console.log('----------------------- newScreens', JSON.stringify(newScreens, null, 4));
-        newScreens.sort((a, b) => parseInt(a[1]) - parseInt(b[1]));
-        return newScreens;
+        function sortScreenEntries(screens) {
+            const normalized = normalizeScreens(screens);
+            const newScreens = extractMinWidths2(normalized);
+            //newScreens.push(['xsm', '501px']); // This smallest screen allowed by Chrome.
+            newScreens.push(['sm0', '501px']); // This smallest screen allowed by Chrome.
+            console.log('----------------------- newScreens', JSON.stringify(newScreens, null, 4));
+            newScreens.sort((a, b) => parseInt(a[1]) - parseInt(b[1]));
+            return newScreens;
+        }
+
+        screenEntries
+            .filter(([screen]) => !ignoredScreens.includes(screen))
+            .forEach(([screen, size]) => {
+                components[`@screen ${screen}`] = {
+                    [`${selector}::before`]: {
+                        content: `'${prefix}${screen} (${size})'`,
+                    },
+                };
+            });
+
+        addComponents(components);
+    },
+    {
+        theme: {
+            debugScreens: {
+                screens: {
+                    'sm0': '501px',
+                }
+            }
+        }
     }
-
-    screenEntries
-        .filter(([screen]) => !ignoredScreens.includes(screen))
-        .forEach(([screen, size]) => {
-            components[`@screen ${screen}`] = {
-                [`${selector}::before`]: {
-                    content: `'${prefix}${screen} (${size})'`,
-                },
-            };
-        });
-
-    addComponents(components);
-});
+);
 
 function resolveValue({ 'min-width': _minWidth, min = _minWidth, max, raw }) {
-    return { min, max, raw }
-  }
+    return { min, max, raw };
+}
 
 function normalizeScreens(screens, root = true): { name: any; not: boolean; values: any[]; }[] {
     if (Array.isArray(screens)) {
@@ -104,14 +115,14 @@ function normalizeScreens(screens, root = true): { name: any; not: boolean; valu
 
 function extractMinWidths(breakpoints: any[] = []) {
     return breakpoints
-      .flatMap((breakpoint) => breakpoint.values.map((breakpoint) => breakpoint.min))
-      .filter((v) => v !== undefined)
+        .flatMap((breakpoint) => breakpoint.values.map((breakpoint) => breakpoint.min))
+        .filter((v) => v !== undefined);
 }
 
 function extractMinWidths2(breakpoints: any[] = []) {
     return breakpoints
-      .flatMap((breakpoint) => breakpoint.values.map((brk) => [breakpoint.name, brk.min]))
-      .filter((v) => v !== undefined)
+        .flatMap((breakpoint) => breakpoint.values.map((brk) => [breakpoint.name, brk.min]))
+        .filter((v) => v !== undefined);
 }
 
 /*
@@ -155,98 +166,98 @@ function extractMinWidths2(breakpoints = []) {
 ]
 */
 
-  /*
+/*
 ----------------------- screenEntries [
-  [ 'sm', '640px' ],
-  [ 'md', '768px' ],
-  [ 'lg', '1024px' ],
-  [ 'xl', '1350px' ],
-  [ '2xl', '1536px' ],
-  [ 'xs', '420px' ],
-  [ '3xl', '1920px' ]
+[ 'sm', '640px' ],
+[ 'md', '768px' ],
+[ 'lg', '1024px' ],
+[ 'xl', '1350px' ],
+[ '2xl', '1536px' ],
+[ 'xs', '420px' ],
+[ '3xl', '1920px' ]
 ]
 ----------------------- screens {
-  sm: '640px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1350px',
-  '2xl': '1536px',
-  xs: '420px',
-  '3xl': '1920px'
+sm: '640px',
+md: '768px',
+lg: '1024px',
+xl: '1350px',
+'2xl': '1536px',
+xs: '420px',
+'3xl': '1920px'
 }
 ----------------------- normalizeScreens [
-    {
-        "name": "sm",
-        "not": false,
-        "values": [
-            {
-                "min": "640px"
-            }
-        ]
-    },
-    {
-        "name": "md",
-        "not": false,
-        "values": [
-            {
-                "min": "768px"
-            }
-        ]
-    },
-    {
-        "name": "lg",
-        "not": false,
-        "values": [
-            {
-                "min": "1024px"
-            }
-        ]
-    },
-    {
-        "name": "xl",
-        "not": false,
-        "values": [
-            {
-                "min": "1350px"
-            }
-        ]
-    },
-    {
-        "name": "2xl",
-        "not": false,
-        "values": [
-            {
-                "min": "1536px"
-            }
-        ]
-    },
-    {
-        "name": "xs",
-        "not": false,
-        "values": [
-            {
-                "min": "420px"
-            }
-        ]
-    },
-    {
-        "name": "3xl",
-        "not": false,
-        "values": [
-            {
-                "min": "1920px"
-            }
-        ]
-    }
+  {
+      "name": "sm",
+      "not": false,
+      "values": [
+          {
+              "min": "640px"
+          }
+      ]
+  },
+  {
+      "name": "md",
+      "not": false,
+      "values": [
+          {
+              "min": "768px"
+          }
+      ]
+  },
+  {
+      "name": "lg",
+      "not": false,
+      "values": [
+          {
+              "min": "1024px"
+          }
+      ]
+  },
+  {
+      "name": "xl",
+      "not": false,
+      "values": [
+          {
+              "min": "1350px"
+          }
+      ]
+  },
+  {
+      "name": "2xl",
+      "not": false,
+      "values": [
+          {
+              "min": "1536px"
+          }
+      ]
+  },
+  {
+      "name": "xs",
+      "not": false,
+      "values": [
+          {
+              "min": "420px"
+          }
+      ]
+  },
+  {
+      "name": "3xl",
+      "not": false,
+      "values": [
+          {
+              "min": "1920px"
+          }
+      ]
+  }
 ]
 ----------------------- extractMinWidths [
-    "640px",
-    "768px",
-    "1024px",
-    "1350px",
-    "1536px",
-    "420px",
-    "1920px"
+  "640px",
+  "768px",
+  "1024px",
+  "1350px",
+  "1536px",
+  "420px",
+  "1920px"
 ]
 */
 
