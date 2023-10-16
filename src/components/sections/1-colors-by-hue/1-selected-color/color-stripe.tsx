9e65fbf38027eb258c0e6b01c6baf6289bfe1f9e
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-15 19:17:28 -0700

    WIP. 1-colors-by-hue. move LockButton to separate file. animations complete later

--- src/components/sections/1-colors-by-hue/1-selected-color/color-stripe.tsx
+++ src/components/sections/1-colors-by-hue/1-selected-color/color-stripe.tsx
@@ -1,11 +1,10 @@
 import { HTMLAttributes } from 'react';
 import { useAtom, useAtomValue } from 'jotai';
-import { a, config, easings, useSpring, useTransition } from '@react-spring/web';
+import { a, easings, useTransition } from '@react-spring/web';
 import { viewHueAtoms } from '@/store';
 import { HueSlider } from './hue-slider';
+import { LockButton } from './lock-button';
 import { classNames } from '@/utils';
-import { IconLockLocked, IconLockUnlocked } from '@/components/ui/icons/normal';
-import { Button } from '@/components/ui/shadcn';
 
 function HueToleranceInfo({ className }: HTMLAttributes<HTMLDivElement>) {
     const [hue, setHue] = useAtom(viewHueAtoms.hueAtom); //TODO: const [localHue, setLocalHue] = useState(0);
@@ -28,85 +27,6 @@ function HueToleranceInfo({ className }: HTMLAttributes<HTMLDivElement>) {
         </div>
     );
 }
-
-function LockButton({ className }: HTMLAttributes<HTMLDivElement>) {
-    const [locked, setLocked] = useAtom(viewHueAtoms.lockedAtom);
-    // const transitions = useTransition(locked, {
-    //     from: { x:0, opacity: 0, 
-    //  },
-    //     enter: { x:0, opacity: 1, 
-    //  },
-    //     leave: { x:0, opacity: 0, 
-    //         config: { duration: 0, easing: easings.easeOutQuad },
-    //         //config: { duration: 200, easing: easings.easeOutQuad },
-    //     },
-    // });
-
-    const transitions = useTransition(Number(locked), {
-        from: {
-          opacity: 0,
-          transform: 'translateX(200%) rotateY(180deg)',
-        },
-        enter: {
-          opacity: 1,
-          transform: 'translateX(0%) rotateY(0deg)',
-        },
-        leave: {
-          opacity: 0,
-          transform: 'translateX(-200%) rotateY(0deg)',
-        },
-        config: config.molasses,
-      });    
-    return (
-        <Button variant={'outline'} size={'sm'} className={classNames("w-12 px-0 py-0 self-center flex overflow-hidden", className)} onClick={() => setLocked((v) => !v)}>
-            {transitions((styles, item) => (
-                item
-                    ? <a.div style={styles}> <IconLockLocked className="w-3 h-3" /> </a.div>
-                    : <a.div style={styles}> <IconLockUnlocked className="w-3 h-3" /> </a.div>
-            ))}
-        </Button>
-    );
-}
-
-// transform: 'translate3d(100%,0,50%)',
-// transform: 'translate3d(0%,0,0)',
-// transform: 'translate3d(-200%,0,50%)',
-
-// function LockButton({ className }: HTMLAttributes<HTMLDivElement>) {
-//     const [locked, setLocked] = useAtom(viewHueAtoms.lockedAtom);
-//     const transitions = useTransition(Number(locked), {
-//         from: { y: '-100%', },
-//         enter: { y: '0', },
-//         leave: {
-//             y: '-100%',
-//             config: { duration: 200, easing: easings.easeOutQuad },
-//         },
-//     });
-//     return (
-//         <Button className={classNames("relative w-3 h-3 flex items-center justify-center", className)} onClick={() => setLocked((v) => !v)}>
-//             {transitions((styles, item) => (
-//                 item
-//                     ? <a.div style={styles}> <IconLockLocked className="absolute left-0 top-0 w-3 h-3" /> </a.div>
-//                     : <a.div style={styles}> <IconLockUnlocked className="absolute left-0 top-0 w-3 h-3" /> </a.div>
-//             ))}
-//         </Button>
-//     );
-// }
-
-{/* <div
-className={classNames(
-    "w-3 h-3 flex items-center justify-center rounded-full cursor-pointer",
-    locked ? "bg-primary-400" : "bg-background border border-border",
-    className,
-)}
-onClick={() => setLocked((v) => !v)}
->
-<div className="w-3 h-3">
-    <IconLockLocked />
-    <IconLockUnlocked />
-</div>
-</div> */}
-
 
 export function ColorStripe() {
     const mono = useAtomValue(viewHueAtoms.monoAtom);
