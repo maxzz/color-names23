30a214cd7e2d131c00d5fd6503bbd9620ae3a05b
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-18 14:51:30 -0700

    WIP. MonoSwitch. alternative animation. 5. done. code clean up

--- src/components/sections/1-colors-by-hue/1-top-panel/mono-color-switch.tsx
+++ src/components/sections/1-colors-by-hue/1-top-panel/mono-color-switch.tsx
@@ -20,23 +20,10 @@ function MonoSwitchCell({ label, active, toLeft, setActive, className, ...rest }
             opacity: 0,
         },
         to: {
-            transform: `scale(${active ? 1 : 0}, 1)`,
-            scale: 1,
+            transform: `scale(${active ? 0 : 1}, 1)`,
             opacity: 1,
         },
-        // kind of OK
-        // from: { x: toLeft ? '100%' : '-100%', opacity: 0, },
-        // to: {
-        //     x: active ? toLeft ? '100%' : '-100%' : '0%',
-        //     // x: toLeft
-        //     //     ? active ? '100%': '0%'
-        //     //     : active ? '-100%': '0%',
-        //     opacity: 1,
-        //     // opacity: toLeft
-        //     //     ? active ? 1 : 0
-        //     //     : active ? 0 : 1,
-        // },
-        config: { duration: !active ? 1250 : 0 }
+        config: { duration: !active ? 150 : 0 }
     });
     return (
         <div
@@ -51,9 +38,7 @@ function MonoSwitchCell({ label, active, toLeft, setActive, className, ...rest }
         >
             {label}
 
-            <a.div style={styles} className={`absolute top-0 left-0 w-full h-full ${toLeft ? 'origin-left' : 'origin-right'} bg-red-500/60`}></a.div>
-            {/* <a.div style={styles} className="absolute top-0 left-0 w-full h-full bg-muted-foreground/40"></a.div> */}
-            {/* <a.div style={styles} className="absolute top-0 left-0 w-full h-full bg-red-500/60"></a.div> */}
+            <a.div style={styles} className={`absolute inset-0 ${toLeft ? 'origin-right' : 'origin-left'} bg-muted-foreground/40 dark:bg-muted-foreground/50`}></a.div>
         </div>
     );
 }
@@ -62,11 +47,6 @@ const MonoSwitchClasses = "max-w-fit relative bg-secondary ring-1 ring-primary-4
 
 export function MonoSwitch({ className }: HTMLAttributes<HTMLDivElement>) {
     const [mono, setMono] = useAtom(viewHueAtoms.monoAtom);
-    const styles = useSpring({
-        from: { x: '0%', },
-        to: { x: mono ? '-100%' : '100%', },
-        config: { duration: 1250 }
-    });
     return (
         <div className={classNames(MonoSwitchClasses, className)}>
             <div className="flex">
@@ -78,7 +58,7 @@ export function MonoSwitch({ className }: HTMLAttributes<HTMLDivElement>) {
                     setActive={() => setMono((v) => !v)}
                 />
                 <MonoSwitchCell
-                    label="Color1111111............11"
+                    label="Color"
                     title="Hue"
                     className="border-l border-primary-400"
                     toLeft={true}
@@ -86,11 +66,6 @@ export function MonoSwitch({ className }: HTMLAttributes<HTMLDivElement>) {
                     setActive={() => setMono((v) => !v)}
                 />
             </div>
-
-            {/* <a.div
-                style={styles}
-                className={classNames("absolute bottom-0 w-1/2 h-full bg-muted-foreground/40")}
-            /> */}
         </div>
     );
 }
