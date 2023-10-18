a8c1b1176669c6d81bd9c819e093cbc263cc6a1a
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-18 13:47:55 -0700

    WIP. MonoSwitch. alternative animation. 1

--- src/components/sections/1-colors-by-hue/1-top-panel/index.tsx
+++ src/components/sections/1-colors-by-hue/1-top-panel/index.tsx
@@ -1,14 +1,16 @@
-import { MonoSwitch } from '../mono-color-switch';
-import { HueColorsStripe } from './color-stripe';
+import { MonoSwitch } from './mono-color-switch';
+import { HueColorsStripe } from './hue-colors-stripe';
 import { CurrentColorValues } from './color-namespace-buttons';
 
 export function TopPanel() {
     return (
         <div className="bg-secondary border-border border-b">
-            <div className="mx-auto p-4 pt-1 max-w-[42rem] grid grid-cols-[90px,minmax(0,1fr),90px] gap-x-4">
+            {/* TODO:  button selector should be 'hue colors' instead of 'hue groups' and 'hue wheel' */}
+            <div className="mx-auto p-4 pt-2 max-w-[42rem] grid grid-cols-[90px,minmax(0,1fr),90px] gap-x-4">
 
-                <MonoSwitch className="place-self-start justify-self-end" />
+                <MonoSwitch className="col-span-3 place-self-start justify-self-end" />
                 {/* TODO: selector: linear/whell */}
+                {/* TODO: add spikes */}
 
                 <HueColorsStripe className="col-span-full h-16" />
 
---
+++ src/components/sections/1-colors-by-hue/1-top-panel/mono-color-switch.tsx
@@ -0,0 +1,72 @@
+import { HTMLAttributes } from "react";
+import { useAtom } from "jotai";
+import { a, useSpring } from "@react-spring/web";
+import { viewHueAtoms } from "@/store";
+import { classNames } from "@/utils";
+
+type MonoSwitchCellProps = {
+    label: string;
+    active: boolean;
+    setActive: () => void;
+};
+
+const shadowClasses = "shadow-[inset_1px_2px_5px_0px_#0004,inset_-0px_-2px_2px_0px_#fffa] opacity-50"
+
+function MonoSwitchCell({ label, active, setActive, className, ...rest }: MonoSwitchCellProps & HTMLAttributes<HTMLDivElement>) {
+    const styles = useSpring({
+        from: { x: '0%', opacity: 0, config: { duration: active ? 1250 : 0 }, },
+        to: { x: !active ? '-100%' : '100%', opacity: active ? 0 : 1, config: { duration: !active ? 1250 : 0 } },
+        //config: { duration: !active ? 1250 : 0 }
+    });
+    return (
+        <div
+            className={classNames(
+                "relative px-2 py-2 flex-0 flex items-end z-10",
+                active ? "bg-background font-bold" : shadowClasses,
+                // TODO: substitute font-bold with alternative solution
+                className,
+            )}
+            onClick={setActive}
+            {...rest}
+        >
+            {label}
+
+            <a.div style={styles} className="absolute top-0 left-0 w-full h-full bg-red-500/60"></a.div>
+        </div>
+    );
+}
+
+const MonoSwitchClasses = "max-w-fit relative bg-secondary ring-1 ring-primary-400 rounded text-xs shadow select-none cursor-pointer overflow-hidden";
+
+export function MonoSwitch({ className }: HTMLAttributes<HTMLDivElement>) {
+    const [mono, setMono] = useAtom(viewHueAtoms.monoAtom);
+    const styles = useSpring({
+        from: { x: '0%', },
+        to: { x: mono ? '-100%' : '100%', },
+        config: { duration: 1250 }
+    });
+    return (
+        <div className={classNames(MonoSwitchClasses, className)}>
+            <div className="flex">
+                <MonoSwitchCell
+                    label="Mono"
+                    title="Monochrome"
+                    active={mono}
+                    setActive={() => setMono((v) => !v)}
+                />
+                <MonoSwitchCell
+                    label="Color1111111............11"
+                    title="Hue"
+                    className="border-l border-primary-400"
+                    active={!mono}
+                    setActive={() => setMono((v) => !v)}
+                />
+            </div>
+
+            {/* <a.div
+                style={styles}
+                className={classNames("absolute bottom-0 w-1/2 h-full bg-muted-foreground/40")}
+            /> */}
+        </div>
+    );
+}
