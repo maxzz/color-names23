f3590ba33da8871ea8ac62281e730836b76dc0fc
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-21 18:29:18 -0700

    WIP. Section4_Chadcn. transformed to grid

--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -5,8 +5,8 @@ import { useSnapshot } from "valtio";
 import { shadcnPalette } from "@/store/4-shadcn";
 
 function SingleColor({ label, color }: { label: string; color: string; }) {
-    return (
-        <Label>
+    return (<>
+        {/* <Label className="flex items-center"> */}
             <div className="pb-1 text-foreground/70 dark:text-foreground/50">
                 {label}
             </div>
@@ -14,25 +14,26 @@ function SingleColor({ label, color }: { label: string; color: string; }) {
                 <Input value={color} onChange={(e) => { }} />
                 <div className="aspect-square h-9 rounded" style={{ backgroundColor: `hsl(${color})` }}></div>
             </div>
-        </Label>
+        {/* </Label> */}
+        </>
     );
 }
 
-function PairColors({ color1, color2 }: { color1: string; color2: string; }) {
-    return (
-        <div className="flex items-center justify-between">
-            <SingleColor label="text" color="red" />
-            <SingleColor label="background" color="red" />
-        </div>
-    );
-}
+// function PairColors({ color1, color2 }: { color1: string; color2: string; }) {
+//     return (
+//         <div className="flex items-center justify-between">
+//             <SingleColor label="text" color="red" />
+//             <SingleColor label="background" color="red" />
+//         </div>
+//     );
+// }
 
 export function Section4_Chadcn({ className }: HTMLAttributes<HTMLUListElement>) {
     const snap = useSnapshot(shadcnPalette);
     const items = snap.vars.vars;
     return (
         <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)}>
-            <div className="container max-w-md mx-auto flex flex-col space-y-4">
+            <div className="container max-w-md mx-auto grid grid-cols-2 gap-2">
                 {/* <PairColors color1="red" color2="red" /> */}
                 {/* <SingleColor label="text" color={snap} /> */}
                 {items.map(([name, color]) => (
