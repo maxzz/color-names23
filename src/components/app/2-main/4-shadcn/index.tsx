dfdd779299d5d789ef1426c5d6e0bda5af93d531
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-21 18:35:53 -0700

    WIP. Section4_Chadcn. add room for the second colors group

--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import { Fragment, HTMLAttributes } from "react";
 import { Input, Label } from "@/components/ui/shadcn";
 import { classNames } from "@/utils";
 import { useSnapshot } from "valtio";
@@ -7,15 +7,15 @@ import { shadcnPalette } from "@/store/4-shadcn";
 function SingleColor({ label, color }: { label: string; color: string; }) {
     return (<>
         {/* <Label className="flex items-center"> */}
-            <div className="pb-1 text-sm text-foreground/70 dark:text-foreground/50 flex items-center">
-                {label}
-            </div>
-            <div className="flex items-center space-x-2">
-                <Input value={color} onChange={(e) => { }} />
-                <div className="aspect-square h-9 rounded" style={{ backgroundColor: `hsl(${color})` }}></div>
-            </div>
+        <div className="pb-1 text-sm text-foreground/70 dark:text-foreground/50 flex items-center">
+            {label}
+        </div>
+        <div className="flex items-center space-x-2">
+            <Input value={color} onChange={(e) => { }} />
+            <div className="aspect-square h-9 rounded" style={{ backgroundColor: `hsl(${color})` }}></div>
+        </div>
         {/* </Label> */}
-        </>
+    </>
     );
 }
 
@@ -33,11 +33,16 @@ export function Section4_Chadcn({ className }: HTMLAttributes<HTMLUListElement>)
     const items = snap.vars.vars;
     return (
         <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)}>
-            <div className="container mx-auto max-w-md grid grid-cols-2 gap-2">
+            <div className="container mx-auto max-w-md grid grid-cols-4 gap-2">
                 {/* <PairColors color1="red" color2="red" /> */}
                 {/* <SingleColor label="text" color={snap} /> */}
                 {items.map(([name, color]) => (
-                    <SingleColor key={name} label={name} color={color} />
+                    <Fragment key={name}>
+                        <SingleColor label={name} color={color} />
+                        
+                        <div className=""></div>
+                        <div className=""></div>
+                    </Fragment>
                 ))}
             </div>
         </div>
