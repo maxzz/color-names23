87fd5ca18d48a820b9402c41caa73dcc1439774f
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-22 14:31:30 -0700

    WIP. store. now convertThemeVars() can handle mutilpe themes

--- src/store/4-shadcn/convert-vars-to-valtio.ts
+++ src/store/4-shadcn/convert-vars-to-valtio.ts
@@ -17,35 +17,34 @@ function groupByForeAndBack(vars: CssVarNameValue[]): ForeAndBack[] {
     return rv;
 }
 
-export function convertDefaultVarsToArray(fileVars: FileThemeVars): OneThemeVars {
-    const rootFirst = Object.entries(fileVars);
-    if (!rootFirst.length) {
-        throw new Error('FileThemeVars is empty');
-    }
-
+export function convertThemeVars(fileVars: FileThemeVars): OneThemeVars[] {
     const rv: OneThemeVars[] = [];
 
-    const [varsName, varsValues] = rootFirst[0];
-    const varsValuesPairs = Object.entries(varsValues);
+    Object.entries(fileVars).map((entry) => {
+        const [varsName, varsValues] = entry;
+        const varsValuesPairs = Object.entries(varsValues);
 
-    const vars = varsValuesPairs
-        .map(([name, color], idx) => {
-            const m = name.match(/^--([^-]+)(-foreground)?$/);
-            if (!m) {
-                throw new Error(`Invalid css var name: ${name}`);
-            }
+        const vars = varsValuesPairs
+            .map(([name, color], idx) => {
+                const m = name.match(/^--([^-]+)(-foreground)?$/);
+                if (!m) {
+                    throw new Error(`Invalid css var name: ${name}`);
+                }
 
-            const [, nameWoDash, fore] = m;
-            return {
-                name: nameWoDash,
-                fore: !!fore,
-                value: color,
-                order: idx,
-                id: uuid.asRelativeNumber(),
-            };
-        });
-    return {
-        name: varsName,
-        vars: groupByForeAndBack(vars),
-    };
+                const [, nameWoDash, fore] = m;
+                return {
+                    name: nameWoDash,
+                    fore: !!fore,
+                    value: color,
+                    order: idx,
+                    id: uuid.asRelativeNumber(),
+                };
+            });
+        return {
+            name: varsName,
+            vars: groupByForeAndBack(vars),
+        };
+    });
+
+    return rv;
 }
