9c84a860b8177022d7c9585154144fb10e4af4f7
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-22 18:33:23 -0700

    WIP. Section4_Chadcn. ring vs. border

--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -10,18 +10,19 @@ function ColorPreview({ color }: { color?: string; }) {
     return (<>
         {color
             ?
-            <div className="flex-none aspect-square w-9 h-9 rounded border-border border-2 overflow-hidden" style={{ backgroundColor: `hsl(${color})` }}></div>
+            <div className="flex-none aspect-square w-9 h-9 ring-border ring-2 rounded overflow-hidden" style={{ backgroundColor: `hsl(${color})` }}></div>
             :
-            <IconNoColor className="aspect-square w-9 h-9 stroke-none bg-neutral-100 fill-neutral-400 rounded border-border border-2 overflow-hidden" />
+            <IconNoColor className="aspect-square w-9 h-9 stroke-none bg-neutral-100 fill-neutral-400 ring-border ring-2 rounded overflow-hidden" />
         }
     </>);
 }
 
 function SingleColor({ foreAndBack }: { foreAndBack: ForeAndBack; }) {
     return (<>
-        <div className="pb-1 text-sm text-foreground/70 dark:text-foreground/50 flex items-center">
+        <div className="mr-4 text-sm text-foreground/70 dark:text-foreground/50 flex items-center">
             {foreAndBack.foreground?.name || foreAndBack.background?.name}
         </div>
+
         {foreAndBack.background
             ? (
                 <div className="flex items-center space-x-2">
@@ -31,18 +32,7 @@ function SingleColor({ foreAndBack }: { foreAndBack: ForeAndBack; }) {
             )
             : <ColorPreview />
         }
-        {/* {foreAndBack.background
-            ? (
-                <div className="flex items-center space-x-2">
-                    <Input value={foreAndBack.background.value} onChange={(e) => { }} />
-                    <div className="flex-none aspect-square w-9 h-9 rounded border-border border-2 overflow-hidden" style={{ backgroundColor: `hsl(${foreAndBack.background.value})` }}></div>
-                </div>
-            )
-            : <div className="aspect-square w-9 h-9 rounded border-border border-2 overflow-hidden">
-                <IconNoColor className="stroke-none bg-neutral-100 fill-neutral-400" />
-            </div>
-        } */}
-
+        
         {foreAndBack.foreground
             ? (
                 <div className="flex items-center space-x-2">
@@ -52,17 +42,6 @@ function SingleColor({ foreAndBack }: { foreAndBack: ForeAndBack; }) {
             )
             : <ColorPreview />
         }
-        {/* {foreAndBack.foreground
-            ? (
-                <div className="flex items-center space-x-2">
-                    <div className="flex-none aspect-square w-9 h-9 rounded border-border border-2 overflow-hidden" style={{ backgroundColor: `hsl(${foreAndBack.foreground.value})` }}></div>
-                    <Input value={foreAndBack.foreground.value} onChange={(e) => { }} />
-                </div>
-            )
-            : <div className="aspect-square w-9 h-9 rounded border-border border-2 overflow-hidden">
-                <IconNoColor className="stroke-none bg-neutral-100 fill-neutral-400" />
-            </div>
-        } */}
     </>);
 }
 
@@ -80,7 +59,7 @@ export function Section4_Chadcn({ className }: HTMLAttributes<HTMLUListElement>)
     const items = snap.vars.vars;
     return (
         <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)}>
-            <div className="container mx-auto max-w-xl grid grid-cols-[auto,1fr,1fr] gap-x-4 gap-y-2">
+            <div className="container mx-auto max-w-xl grid grid-cols-[auto,1fr,1fr] gap-x-2 gap-y-2">
                 <Header />
                 {items.map((foreAndBack, idx) => (
                     <SingleColor foreAndBack={foreAndBack} key={idx} />
