c8d869961ebd8cfed999adf1475d379203ee810e
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-22 18:04:07 -0700

    WIP. Section4_Chadcn. alternative no-color placeholder

--- src/components/ui/icons/normal/04-no-color.tsx
+++ src/components/ui/icons/normal/04-no-color.tsx
@@ -5,11 +5,24 @@ export function IconNoColor(props: SVGProps<SVGSVGElement> & HTMLAttributes<SVGS
     return (
         <svg fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 21h3v-3H3v3Zm5 0h3v-3H8v3Zm5 0h3v-3h-3v3Zm5 0h3v-3h-3v3ZM3 6h3V3H3v3Zm0 5h3V8H3v3Zm0 5h3v-3H3v3ZM8 6h3V3H8v3Zm0 5h3V8H8v3Zm0 5h3v-3H8v3Zm5-10h3V3h-3v3Zm0 5h3V8h-3v3Zm0 5h3v-3h-3v3Zm5-10h3V3h-3v3Zm0 5h3V8h-3v3Zm0 5h3v-3h-3v3Z"
-            />
+            <path d="M0 .01h4v4H0z" />
+            <path d="M4 4.01h4v4H4z" />
+            <path d="M8 .01h4v4H8z" />
+            <path d="M12 4.01h4v4h-4z" />
+            <path d="M16 .01h4v4h-4z" />
+            <path d="M20 4.01h4v4h-4z" />
+            <path d="M4 20h4v4H4z" />
+            <path d="M0 16h4v4H0z" />
+            <path d="M4 12.01h4v4H4z" />
+            <path d="M0 8.01h4v4H0z" />
+            <path d="M8 8.01h4v4H8z" />
+            <path d="M12 12.01h4v4h-4z" />
+            <path d="M16 8.01h4v4h-4z" />
+            <path d="M20 12.01h4v4h-4z" />
+            <path d="M12 20h4v4h-4z" />
+            <path d="M8 16h4v4H8z" />
+            <path d="M16 16h4v4h-4z" />
+            <path d="M20 20h4v4h-4z" />
         </svg>
     );
 }
