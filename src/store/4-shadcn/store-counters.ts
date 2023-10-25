b318be10d3a2fd0dc79658428bb1812a29e9f9fd
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-25 15:48:23 -0700

    WIP. store. fix fore/back counters in store-counters.tsx

---
+++ src/store/4-shadcn/store-counters.ts
@@ -0,0 +1,53 @@
+import { proxy, subscribe } from "valtio";
+import { ColorCounters, OneThemeVars } from "./types";
+import { shadcnPalette } from "./store-palette";
+
+function makeColorCounters(vars: OneThemeVars): Record<string, number> {
+    const rv = new Map();
+
+    function checkColor(color: string | undefined) {
+        if (!color) {
+            return;
+        }
+        if (!rv.has(color)) {
+            rv.set(color, 0);
+        }
+        rv.set(color, rv.get(color) + 1);
+    }
+
+    vars.vars.reduce((acc, fb) => {
+        checkColor(fb.b?.value);
+        checkColor(fb.f?.value);
+        return acc;
+    }, rv);
+
+    return Object.fromEntries(rv);
+}
+
+// function makeColorCounters(vars: OneThemeVars): Record<string, number> {
+//     const rv = new Map();
+
+//     vars.vars.reduce((acc, fb) => {
+//         const color = fb.b?.value || fb.f?.value;
+//         if (!color) {
+//             return acc;
+//         }
+//         if (!rv.has(color)) {
+//             rv.set(color, 0);
+//         }
+//         rv.set(color, rv.get(color) + 1);
+//         return acc;
+//     }, rv);
+
+//     return Object.fromEntries(rv);
+// }
+
+export const colorCounters = proxy<ColorCounters>({
+    counters: makeColorCounters(shadcnPalette.varGroups),
+});
+console.log('colorCounters', colorCounters.counters);
+
+subscribe(shadcnPalette.varGroups, () => {
+    colorCounters.counters = makeColorCounters(shadcnPalette.varGroups);
+    console.log('shadcnPalette.vars changed', colorCounters.counters);
+});
