18689d0b2b8036bddf29c03ecfefe890c9c655b9
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-25 16:07:50 -0700

    WIP. Switch. rollback experimants w/ pseudo bold

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, HTMLAttributes, SetStateAction } from "react";
+import { HTMLAttributes, SetStateAction } from "react";
 import { a, useSpring } from "@react-spring/web";
 import { classNames } from "@/utils";
 
@@ -9,14 +9,9 @@ type SwitchCellProps = {
     setActive: () => void;
 };
 
-const shadowDarkClasses = "dark:shadow-[inset_2px_2px_5px_0.2px_#000000,inset_-0px_-2px_2px_0px_#e6e6e638]";
-const shadowLightClasses = "shadow-[inset_1px_2px_5px_0px_#0004,inset_-0px_-2px_2px_0px_#fffa]";
-const shadowClasses = `${shadowLightClasses} ${shadowDarkClasses}`; //TODO: opacity should go to a separate div, not to text
-
-// const pseudoBold: CSSProperties = {
-//     WebkitTextStroke: "0.5px red",
-//     //color: "rgb(102 116 161)",
-// };
+const shadowDarkClasses = "dark:shadow-[inset_2px_2px_5px_0.2px_#000000,inset_-0px_-2px_2px_0px_#e6e6e638] opacity-50";
+// const shadowClasses = "shadow-[inset_2px_2px_5px_0.2px_#000000,inset_-0px_-2px_2px_0px_#e6e6e638] opacity-50";
+const shadowLightClasses = "shadow-[inset_1px_2px_5px_0px_#0004,inset_-0px_-2px_2px_0px_#fffa] opacity-50";
 
 function SwitchCell({ label, active, toLeft, setActive, className, ...rest }: SwitchCellProps & HTMLAttributes<HTMLDivElement>) {
     const styles = useSpring({
@@ -34,32 +29,14 @@ function SwitchCell({ label, active, toLeft, setActive, className, ...rest }: Sw
         <div
             className={classNames(
                 "relative px-2 py-2 flex-0 flex items-end z-10",
-                active ? "bg-background" : shadowClasses,
+                active ? "bg-background font-bold" : `${shadowLightClasses} ${shadowDarkClasses}}`,
                 // TODO: substitute font-bold with alternative solution
-                "",
                 className,
             )}
-            // style={active ? {} : pseudoBold}
             onClick={setActive}
             {...rest}
         >
-            <div
-                className={classNames(
-                    // "[-webkit-text-stroke:.5px_red]", // OK
-                    // "[-webkit-text-stroke:.5px_bg-muted-foreground/40 dark:bg-muted-foreground/50]", // NOT OK
-                    // "text-foreground",
-                    active ? "text-foreground" : "text-foreground",
-                    //"text-foreground",
-                    // "[-webkit-text-stroke:1.9px_hsl(var(--muted-foreground)_/_0.4)]",
-                    active
-                        ? "[-webkit-text-stroke:.4px_hsl(var(--foreground)_/_1)]"
-                        : "[-webkit-text-stroke:.5px_hsl(var(--muted-foreground)_/_0.4)]"
-                    ,
-                )}
-            >
-                {label}
-            </div>
-            <div className={classNames("absolute inset-0", !active && "bg-muted/40 dark:bg-muted/50")}></div>
+            {label}
 
             <a.div style={styles} className={`absolute inset-0 ${toLeft ? 'origin-right' : 'origin-left'} bg-muted-foreground/40 dark:bg-muted-foreground/50`}></a.div>
         </div>
