9b4f62790c42acf407ab3eefa11ba4ed781813e8
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-25 18:06:54 -0700

    WIP. store. update loading order

--- src/components/app/2-main/1-colors-by-hue/3-page-hue-wheel/color-over-background.tsx
+++ src/components/app/2-main/1-colors-by-hue/3-page-hue-wheel/color-over-background.tsx
@@ -1,7 +1,10 @@
 import { HTMLAttributes } from "react";
+import { useSnapshot } from "valtio";
+import { colorOverBackground } from "@/store";
 import { classNames } from "@/utils";
 
 export function ColorOverBackground({className, ...rest}: HTMLAttributes<HTMLDivElement>) {
+    const snap = useSnapshot(colorOverBackground)
     return (
         <div className={classNames(className)} {...rest}>
             ColorOverBackground
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
-export * from './0-app-settings-jotai/types';
-import './0-app-settings-jotai/store-load';
+export * from './0-app-settings-valtio';
 
+export * from './0-app-settings-jotai/types';
+
+import './0-app-settings-jotai/store-load'; // unfortunately, the order is important here
 export * from './0-app-settings-jotai/store-initial-data';
+
 export * from './0-app-settings-jotai';
 export * from './1-hue';
 export * from './2-sorted-colors';
