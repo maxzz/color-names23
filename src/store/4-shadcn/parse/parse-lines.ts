95ca47b332639002886cc1ffced77dd102a28b63
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-28 20:54:54 -0700

    WIP. parseTextAsCSSvars(). update exports

--- src/store/4-shadcn/parse/parse-lines.ts
+++ src/store/4-shadcn/parse/parse-lines.ts
@@ -1,13 +1,17 @@
-// <":root": {> or <:root: {> or <"dark": {> or <.dark: {>
-const isThemeNameRegex = /^\s*(['"])?([\.\:]?[a-zA-Z0-9\-]+)(?:['"])?\s*:?\s* \{\s*$/;
-
-// <"--background": "224 71% 4%",> or <--background: 159 65% 4%;>
-const isCSSVarRegex = /^\s*(['"])?--([a-zA-Z0-9\-]+)(?:['"])?\s*:\s*(?:['"])?([^;"']+)(?:['"])?\s*[;,]?\s*$/;
-
-type ThemeVars = {
+export type ThemeVars = {
     name: string;
     values: Record<string, string>;
 };
+
+// ":root": {
+// :root: {
+// "dark": {
+// .dark: {
+const isThemeNameRegex = /^\s*(['"])?([\.\:]?[a-zA-Z0-9\-]+)(?:['"])?\s*:?\s* \{\s*$/;
+
+// "--background": "224 71% 4%",
+// --background: 159 65% 4%;
+const isCSSVarRegex = /^\s*(['"])?--([a-zA-Z0-9\-]+)(?:['"])?\s*:\s*(?:['"])?([^;"']+)(?:['"])?\s*[;,]?\s*$/;
 
 export function parseTextAsCSSvars(text: string): ThemeVars[] {
     let rv: ThemeVars[] = [];
