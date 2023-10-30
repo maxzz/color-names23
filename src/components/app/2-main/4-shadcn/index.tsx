f74b6c231d332ed939df1f976d15f061745dee94
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-29 17:40:58 -0700

    WIP. PasteArea. now inputs share paste data. OK.

--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -5,8 +5,7 @@ import { Header, Header2 } from "./1-headers";
 import { GridRow } from "./2-grid-row";
 import { classNames, cn } from "@/utils";
 import { parseTextAsCSSvars, testToParseCss, testToParseCss2, testToParseJs } from "@/store/4-shadcn/parse";
-import { AutoGrowTextarea, Input, textareaPaddingFontClasses, textareaPaddingFontDebugClasses } from "@/components/ui/shadcn";
-import { Textarea } from "@/components/ui/shadcn/textarea";
+import { AutoGrowTextarea, Input, Textarea, textareaPaddingFontClasses, textareaPaddingFontDebugClasses } from "@/components/ui/shadcn";
 
 // const vars = parseTextAsCSSvars(testToParseCss2);
 // console.log('vars', vars);
@@ -22,7 +21,7 @@ subscribe(parseText, () => {
 
 function PasteArea() {
     const snap = useSnapshot(parseText, { sync: true });
-    return (<div className="py-4 resize-y bg-red-300">
+    return (<>
         <Textarea
             value={snap.text}
             onChange={(e) => parseText.text = e.target.value}
@@ -31,26 +30,25 @@ function PasteArea() {
             placeholder="Paste theme vars here"
             spellCheck={false}
         />
-    </div>);
+                <AutoGrowTextarea
+                    rows={1}
+                    className="min-h-0"
+                    textareaPaddingFont={cn(textareaPaddingFontDebugClasses, textareaPaddingFontClasses)}
+                    value={snap.text}
+                    onChange={(e) => parseText.text = e.target.value}
+                    spellCheck={false}
+                />
+    </>);
 }
 
 export function Section4_Chadcn({ className }: HTMLAttributes<HTMLUListElement>) {
     const { varGroups: { vars: snapItems } } = useSnapshot(shadcnPalette);
     const items = shadcnPalette.varGroups.vars;
-    const [value, setValue] = useState('');
     return (
         <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)}>
 
             <div className="my-4">
                 <PasteArea />
-                <AutoGrowTextarea
-                    rows={1}
-                    className="min-h-[36px]"
-                    // className="min-h-0"
-                    textareaPaddingFont={cn(textareaPaddingFontDebugClasses, textareaPaddingFontClasses)}
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                />
             </div>
 
             <div className="container mx-auto max-w-xl grid grid-cols-[min-content,minmax(0,12rem),minmax(0,12rem)] place-content-center gap-y-2">
