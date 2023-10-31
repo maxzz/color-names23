8a46119be61eb2e927d395a777e39cb31014c42a
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-30 18:35:54 -0700

    WIP. store. switch convertFileThemeVarsToPairs to produce array

--- src/store/4-shadcn/convert-vars-to-valtio.ts
+++ src/store/4-shadcn/convert-vars-to-valtio.ts
@@ -3,6 +3,7 @@ import { uuid } from "@/utils";
 
 function groupByForeAndBack(vars: CssVarNameValue[], combineForeBack: boolean): ForeAndBack[] {
     const map = new Map<string, ForeAndBack>();
+
     vars.forEach((v) => {
         let newForeAndBack = map.get(v.name);
         if (!newForeAndBack) {
@@ -25,41 +26,46 @@ function groupByForeAndBack(vars: CssVarNameValue[], combineForeBack: boolean): 
 
     let rv: ForeAndBack[] = [...map.values()];
     rv = rv.filter((fb) => fb.b || fb.f);
+
     return rv;
 }
 
 const matchFore = /^\s*--([^-]+)(-foreground)?\s*$/;
 const matchHSL = /^\s*(hsl\()?(\d+\.?\d*)\s+(\d+\.?\d*)%\s+(\d+\.?\d*)%(\))?\s*$/;
 
-export function convertThemeVars(fileVars: FileThemeVars): OneThemeVars[] {
-    const rv: OneThemeVars[] = Object.entries(fileVars).map((entry) => {
-        const [varsName, varsValues] = entry;
-        const varsValuesPairs = Object.entries(varsValues);
+export function convertFileThemeVarsToPairs(fileVars: FileThemeVars): OneThemeVars[] {
+    const rv: OneThemeVars[] =
+        Object.entries(fileVars)
+            .map((entry) => {
+                const [varsName, varsValues] = entry;
+                const varsValuesPairs = Object.entries(varsValues);
 
-        const vars = varsValuesPairs
-            .map(([name, color], idx) => {
-                const m = name.match(matchFore);
-                if (!m) {
-                    throw new Error(`Invalid css var name: ${name}. Name should start with '--'`);
-                }
-                const [, nameWoDash, fore] = m;
+                const vars = varsValuesPairs
+                    .map(([name, color], idx) => {
+                        const m = name.match(matchFore);
+                        if (!m) {
+                            throw new Error(`Invalid css var name: ${name}. Name should start with '--'`);
+                        }
+                        const [, nameWoDash, fore] = m;
 
-                const m2 = color.match(matchHSL);
-                const isHsl = !!m2;
+                        const m2 = color.match(matchHSL);
+                        const isHsl = !!m2;
 
+                        return {
+                            name: nameWoDash,
+                            fore: !!fore,
+                            value: color,
+                            isHsl,
+                            order: idx,
+                            id: uuid.asRelativeNumber(),
+                        };
+                    });
                 return {
-                    name: nameWoDash,
-                    fore: !!fore,
-                    value: color,
-                    isHsl,
-                    order: idx,
-                    id: uuid.asRelativeNumber(),
+                    name: varsName,
+                    vars: groupByForeAndBack(vars, true),
                 };
             });
-        return {
-            name: varsName,
-            vars: groupByForeAndBack(vars, true),
-        };
-    });
     return rv;
 }
+
+//TODO: add toaster and catch errors
