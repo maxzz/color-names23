d0863c2dd197829687726ece7fcdd7f18c15435d
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-01 17:05:09 -0700

    WIP. ColorPicker. split monolith. 1

--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -4,7 +4,7 @@ import { shadcnAll } from "@/store";
 import { classNames } from "@/utils";
 import { PasteArea } from "./0-top-panel";
 import { GroupGrid } from "./5-group-grid";
-import { PickerExample } from "@/components/ui/shadcn/color-picker";
+import { PickerExample } from "@/components/ui/shadcn/gradient-color-picker";
 
 export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     const { length } = useSnapshot(shadcnAll.themes);
