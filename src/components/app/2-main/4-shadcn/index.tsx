cfe365c2a2ebda886f545aff1b2002e4ecf1c6e4
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-01 19:24:37 -0700

    WIP. store. hack w/ keys does not work when @ of items in theme changed

--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -6,26 +6,26 @@ import { PasteArea } from "./0-top-panel";
 import { GroupGrid } from "./5-group-grid";
 import { PickerExample } from "@/components/ui/shadcn/gradient-color-picker";
 
-export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    const {themes} = useSnapshot(shadcnAll);
-    //const themesArray = Array(length).fill(0);
-    console.log('themesArray', themes);
+// export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
+//     const {themes} = useSnapshot(shadcnAll);
+//     //const themesArray = Array(length).fill(0);
+//     console.log('themesArray', themes);
     
-    return (
-        <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)} {...rest}>
+//     return (
+//         <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)} {...rest}>
 
-            <div className="my-4">
-                <PasteArea />
-                {/* <PickerExample /> */}
-            </div>
+//             <div className="my-4">
+//                 <PasteArea />
+//                 {/* <PickerExample /> */}
+//             </div>
 
-            {themes.map((_, idx) => (
-                <GroupGrid themeVars={shadcnAll.themes[idx]} idx={idx} key={idx} />
-            ))}
+//             {themes.map((_, idx) => (
+//                 <GroupGrid themeVars={shadcnAll.themes[idx]} idx={idx} key={idx} />
+//             ))}
             
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
 // export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
 //     const { length } = useSnapshot(shadcnAll.themes);
@@ -47,3 +47,24 @@ export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivEl
 //         </div>
 //     );
 // }
+
+export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
+    const { length } = useSnapshot(shadcnAll.themes);
+    const themesArray = Array(length).fill(0);
+    console.log('themesArray', themesArray);
+    
+    return (
+        <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)} {...rest}>
+
+            <div className="my-4">
+                <PasteArea />
+                {/* <PickerExample /> */}
+            </div>
+
+            {themesArray.map((_, idx) => (
+                <GroupGrid themeVars={shadcnAll.themes[idx]} idx={idx} key={shadcnAll.themes[idx].themeId} />
+            ))}
+            
+        </div>
+    );
+}
