00ee08eb1a76506b50f81ea06e9090d25092dadc
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-01 18:27:58 -0700

    WIP. store. fix with use memo for array reactivity

--- src/components/app/2-main/4-shadcn/5-group-grid.tsx
+++ src/components/app/2-main/4-shadcn/5-group-grid.tsx
@@ -5,8 +5,20 @@ import { HeaderColorValues, HeaderLengthValues } from "./1-headers";
 import { GridRow } from "./2-grid-row";
 
 /*
- --primary: 161 56% 35%;
+--primary: 161 56% 35%;
+--background: 159 65% 4%;
+--foreground: 159 10% 97.5%;
+
  --primary: 0 100% 50%;
+
+:root {
+    --primary: 161 56% 35%;
+    --background: 159 65% 4%;
+    --foreground: 159 10% 97.5%;
+}
+.dark {
+    --primary: 0 100% 50%;
+}
 */
 export function GroupGrid({ themeVars, idx }: { themeVars: ThemeVars; idx: number }) {
     const snapT = useSnapshot(shadcnAll.themes);
--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, memo } from "react";
 import { useSnapshot } from "valtio";
 import { shadcnAll } from "@/store";
 import { classNames } from "@/utils";
@@ -6,11 +6,13 @@ import { PasteArea } from "./0-top-panel";
 import { GroupGrid } from "./5-group-grid";
 import { PickerExample } from "@/components/ui/shadcn/gradient-color-picker";
 
+const GroupGridMemo = memo(GroupGrid);
+
 export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    const { length } = useSnapshot(shadcnAll.themes);
+    const themes = useSnapshot(shadcnAll.themes);
     const themesArray = Array(length).fill(0);
     console.log('themesArray', themesArray);
-    
+
     return (
         <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)} {...rest}>
 
@@ -19,10 +21,10 @@ export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivEl
                 {/* <PickerExample /> */}
             </div>
 
-            {themesArray.map((_, idx) => (
-                <GroupGrid themeVars={shadcnAll.themes[idx]} idx={idx} key={idx} />
+            {themes.map((_, idx) => (
+                <GroupGridMemo themeVars={shadcnAll.themes[idx]} idx={idx} key={idx} />
             ))}
-            
+
         </div>
     );
 }
