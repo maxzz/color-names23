721918549a2fe4f3aeddc3c572e3387785be4628
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-03 15:32:30 -0700

    WIP. GroupGrid. alternative params type for splitColorAndLengths()

--- src/components/app/2-main/4-shadcn/5-group-grid.tsx
+++ src/components/app/2-main/4-shadcn/5-group-grid.tsx
@@ -4,16 +4,15 @@ import { ThemeVarFB, shadcnAll } from "@/store";
 import { HeaderColorValues, HeaderLengthValues } from "./1-headers";
 import { GridRow } from "./4-grid-row";
 
-type ThemeVarFBGroup = { idx: number; key: number | string; themeVar: ThemeVarFB; };
+type ThemeVarFBGroup = { idx: number; key: number | string; };
 
-function splitColorAndLengths(vars: INTERNAL_Snapshot<ThemeVarFB[]>) {
+function splitColorAndLengths<T extends INTERNAL_Snapshot<ThemeVarFB[]>>(vars: T) {
     return vars.reduce(
         (acc, curr, idx) => {
             const isColor = curr.b?.isHsl || curr.f?.isHsl;
             acc[isColor ? 'colors' : 'lengths'].push({
                 idx,
                 key: curr.b?.id || curr.f?.id || `${isColor ? 'color' : 'length'}-${idx}}`,
-                themeVar: curr,
             });
             return acc;
         },
@@ -33,46 +32,15 @@ export function GroupGrid({ idx }: { idx: number; }) {
 
             {!!colors.length && (<>
                 <HeaderColorValues />
-                {colors.map(({ key, idx, themeVar }) => (
+                {colors.map(({ key, idx }) => (
                     <GridRow foreAndBack={theRightTheme.vars[idx]} key={key} />
                 ))}
             </>)}
 
             {!!lengths.length && (<>
                 <HeaderLengthValues />
-                {lengths.map(({ key, idx, themeVar }) => (
+                {lengths.map(({ key, idx }) => (
                     <GridRow foreAndBack={theRightTheme.vars[idx]} key={key} />
-                ))}
-            </>)}
-
-        </div>
-    );
-}
-
-export function GroupGrid0({ idx }: { idx: number; }) {
-    const snap = useSnapshot(shadcnAll.themes)[idx];
-    const theRightTheme = shadcnAll.themes[idx];
-    return (
-        <div className="container mx-auto max-w-xl grid grid-cols-[min-content,minmax(0,12rem),minmax(0,12rem)] place-content-center gap-y-2">
-            {/* version w/ keys */}
-            {snap.vars.length && (<>
-
-                <HeaderColorValues />
-                {snap.vars.map((foreAndBack, idx) => (
-                    <Fragment key={`${foreAndBack.b?.id || foreAndBack.f?.id || idx}`}>
-                        {(foreAndBack.b?.isHsl || foreAndBack.f?.isHsl) &&
-                            <GridRow foreAndBack={theRightTheme.vars[idx]} />
-                        }
-                    </Fragment>
-                ))}
-
-                <HeaderLengthValues />
-                {snap.vars.map((foreAndBack, idx) => (
-                    <Fragment key={`${foreAndBack.b?.id || foreAndBack.f?.id || idx}-length`}>
-                        {(!foreAndBack.b?.isHsl && !foreAndBack.f?.isHsl) &&
-                            <GridRow foreAndBack={theRightTheme.vars[idx]} />
-                        }
-                    </Fragment>
                 ))}
             </>)}
 
