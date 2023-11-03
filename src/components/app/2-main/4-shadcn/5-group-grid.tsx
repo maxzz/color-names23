5092eb33b302ab3ac0bf1d2aa82b3e5e3a91669e
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-03 15:08:00 -0700

    WIP. GroupGrid. add splitColorAndLengths()

--- src/components/app/2-main/4-shadcn/5-group-grid.tsx
+++ src/components/app/2-main/4-shadcn/5-group-grid.tsx
@@ -1,12 +1,31 @@
 import { Fragment } from "react";
 import { useSnapshot } from "valtio";
-import { shadcnAll } from "@/store";
+import { ThemeVar, ThemeVarFB, shadcnAll } from "@/store";
 import { HeaderColorValues, HeaderLengthValues } from "./1-headers";
 import { GridRow } from "./4-grid-row";
 
+type ThemeVarFBGroup = { idx: number; key: number; themeVar: ThemeVarFB; };
+
+function splitColorAndLengths(vars: ThemeVarFB[]) {
+    return vars.reduce(
+        (acc, curr, idx) => {
+            const key = curr.b?.id || curr.f?.id || idx;
+            if (curr.b?.isHsl || curr.f?.isHsl) {
+                acc.colors.push({ idx, key, themeVar: curr });
+            } else {
+                acc.lengths.push({ idx, key, themeVar: curr });
+            }
+            return acc;
+        },
+        {
+            colors: [] as ThemeVarFBGroup[],
+            lengths: [] as ThemeVarFBGroup[],
+        }
+    );
+}
+
 export function GroupGrid({ idx }: { idx: number; }) {
-    const snapThemes = useSnapshot(shadcnAll.themes);
-    const snap = snapThemes[idx];
+    const snap = useSnapshot(shadcnAll.themes)[idx];
     const theRightTheme = shadcnAll.themes[idx];
     return (
         <div className="container mx-auto max-w-xl grid grid-cols-[min-content,minmax(0,12rem),minmax(0,12rem)] place-content-center gap-y-2">
