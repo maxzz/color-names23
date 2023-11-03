2c232d3137feac83b1644b8f53994e087630da0e
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-03 16:07:33 -0700

    WIP. Section4_Chadcn. fix TopPanel re-renders

--- src/components/app/2-main/4-shadcn/0-top-panel.tsx
+++ src/components/app/2-main/4-shadcn/0-top-panel.tsx
@@ -4,22 +4,29 @@ import { Button, Textarea } from "@/components/ui/shadcn";
 import { IconMenuBurger } from "@/components/ui/icons";
 import { TestButtons } from "./9-test-buttons";
 
-export function PasteArea() {
+function PasteInput() {
     const snap = useSnapshot(parseText, { sync: true });
+    return (
+        <Textarea
+            value={snap.text}
+            onChange={(e) => parseText.text = e.target.value}
+            className="min-h-[36px] text-xs smallscroll resizer [&::-webkit-resizer]:rounded [&::-webkit-resizer]:[backgroundSize:80%_80%]"
+            rows={1}
+            placeholder="Paste theme vars here"
+            spellCheck={false}
+        />
+    );
+}
+
+export function TopPanel() {
+
     return (
         <div className="-mx-4 px-4 py-4 bg-muted/50 flex items-center space-x-2">
             <Button className="flex-none" variant={"outline"} size={"icon"}>
                 <IconMenuBurger className="w-4 h-4" />
             </Button>
 
-            <Textarea
-                value={snap.text}
-                onChange={(e) => parseText.text = e.target.value}
-                className="min-h-[36px] text-xs smallscroll resizer [&::-webkit-resizer]:rounded [&::-webkit-resizer]:[backgroundSize:80%_80%]"
-                rows={1}
-                placeholder="Paste theme vars here"
-                spellCheck={false}
-            />
+            <PasteInput />
 
             <TestButtons />
         </div>
--- src/components/app/2-main/4-shadcn/index.tsx
+++ src/components/app/2-main/4-shadcn/index.tsx
@@ -2,26 +2,20 @@ import { HTMLAttributes } from "react";
 import { useSnapshot } from "valtio";
 import { shadcnAll } from "@/store";
 import { classNames } from "@/utils";
-import { PasteArea } from "./0-top-panel";
+import { TopPanel } from "./0-top-panel";
 import { GroupGrid } from "./5-group-grid";
 import { PickerExample } from "@/components/ui/shadcn/gradient-color-picker";
+import { ThemeGrids } from "./6-grids";
 
 export function Section4_Chadcn({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    const { length } = useSnapshot(shadcnAll.themes);
     return (
         <div className={classNames("p-4 h-full text-foreground bg-background border-muted border-b overflow-auto smallscroll flex flex-col", className)} {...rest}>
-
             <div className="my-4">
-                <PasteArea />
+                <TopPanel />
                 {/* <PickerExample /> */}
             </div>
 
-            <div className="container mx-auto max-w-xl grid grid-cols-[min-content,minmax(0,12rem),minmax(0,12rem)] place-content-center gap-y-2">
-                {Array(length).fill(0).map((_, idx) => (
-                    <GroupGrid idx={idx} key={shadcnAll.themes[idx].themeId} />
-                ))}
-            </div>
-
+            <ThemeGrids />
         </div>
     );
 }
