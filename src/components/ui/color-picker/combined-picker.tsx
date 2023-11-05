30f4501e02f8dcb7d82f365d51fa2d4ff20bc53e
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-05 13:28:30 -0800

    WIP. color-picker. store hexColor as part of ui-state

--- src/components/ui/color-picker/combined-picker.tsx
+++ src/components/ui/color-picker/combined-picker.tsx
@@ -1,14 +1,13 @@
 import { useCallback } from "react";
 import { Saturation } from "./color-saturation";
 import { HsvaColor, hsvaToHex } from "./color-convert";
-import { debounce } from "@/utils";
 import { PointerCircle } from "./color-saturation/pointer";
 import { useSnapshot } from "valtio";
 import { colorPickerState } from "./ui-state";
 
 function ColorNumbers() {
     const snap = useSnapshot(colorPickerState);
-    const color = hsvaToHex(snap.hsvaColor);
+    const color = snap.hexColor;
     return (
         <div className="flex items-center space-x-2">
             <div className="w-4 h-4" style={{ background: color }}></div>
@@ -20,24 +19,13 @@ function ColorNumbers() {
 export function SaturationSelector() {
     const snap = useSnapshot(colorPickerState);
 
-    // const onColorChange = useCallback(
-    //     debounce((newColor: HsvaColor) => {
-    //         console.log('newColor', newColor);
-    //         colorPickerState.hsvaColor = newColor;
-    //     }, 50), []
-    // );
     const onColorChange = useCallback((newColor: HsvaColor) => {
-        const hexCurrent = hsvaToHex(snap.hsvaColor);
         const hexNew = hsvaToHex(newColor);
-        console.log('compare', hexCurrent, hexNew);
-        if (hexCurrent === hexNew) {
-            console.log('the same', hexCurrent, hexNew);
-            
+        if (colorPickerState.hexColor === hexNew) {
             return;
         }
-
-        console.log('newColor', hsvaToHex(newColor), newColor);
         colorPickerState.hsvaColor = newColor;
+        colorPickerState.hexColor = hexNew;
     }, [snap.hsvaColor]
     );
 
--- src/components/ui/color-picker/ui-state.ts
+++ src/components/ui/color-picker/ui-state.ts
@@ -3,8 +3,10 @@ import { HsvaColor } from "./color-convert";
 
 export type ColorPickerState = {
     hsvaColor: HsvaColor;
+    hexColor: string;
 }
 
 export const colorPickerState = proxy<ColorPickerState>({
     hsvaColor: { h: 0, s: 0, v: 0, a: 1 },
+    hexColor: "#000000",
 });
