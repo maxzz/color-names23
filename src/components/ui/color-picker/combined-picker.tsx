a874f3894a86036ccda057f9cd641825e6a3026f
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-05 18:34:32 -0800

    WIP. color-picker. update sub-view props

--- src/components/ui/color-picker/combined-picker.tsx
+++ src/components/ui/color-picker/combined-picker.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { HTMLAttributes, useCallback } from "react";
 import { Saturation } from "./view-saturation";
 import { hsvaToHex, hsvaToHexa } from "./color-convert";
 import { PointerCircle } from "./part-pointer";
@@ -6,10 +6,91 @@ import { useSnapshot } from "valtio";
 import { colorPickerState } from "./ui-state";
 import { Alpha, PointerCircleAlpha } from "./view-alpha";
 import { Hue } from "./view-hue";
+import { classNames } from "@/utils";
+
+function AlphaView({ className, ...rest }: Omit<HTMLAttributes<HTMLDivElement>, 'onChange'>) {
+    const snap = useSnapshot(colorPickerState);
+    const { h, s, v, a } = snap.hsvaColor;
+
+    const onAlphaChange = useCallback(
+        (a: number) => {
+            if (colorPickerState.hsvaColor.a !== a) {
+                colorPickerState.hsvaColor.a = a;
+            }
+        }, [snap.hsvaColor]
+    );
+
+    return (
+        <Alpha
+            className={classNames("w-56 h-6 border-foreground border", className)}
+            hsv={{ h, s, v }}
+            onChange={onAlphaChange}
+            {...rest}
+        >
+            <PointerCircleAlpha value={a * 100} />
+        </Alpha>
+    );
+}
+
+function HueView({ className, ...rest }: Omit<HTMLAttributes<HTMLDivElement>, 'onChange'>) {
+    const snap = useSnapshot(colorPickerState);
+    const { h } = snap.hsvaColor;
+
+    const onHueChange = useCallback(
+        (h: number) => {
+            if (colorPickerState.hsvaColor.h !== h) {
+                colorPickerState.hsvaColor.h = h;
+            }
+        }, [snap.hsvaColor]
+    );
+
+    return (
+        <Hue
+            className={classNames("w-56 h-6 border-foreground border", className)}
+            hue={h}
+            onChange={onHueChange}
+            {...rest}
+        >
+            <PointerCircleAlpha value={(h / 360) * 100} />
+        </Hue>
+    );
+}
+
+function SaturationView({ className, ...rest }: Omit<HTMLAttributes<HTMLDivElement>, 'onChange'>) {
+    const snap = useSnapshot(colorPickerState);
+
+    const onSaturationValueChange = useCallback(
+        ({ s, v }: { s: number; v: number; }) => {
+            if (colorPickerState.hsvaColor.s !== s || colorPickerState.hsvaColor.v !== v) {
+                colorPickerState.hsvaColor.v = v;
+                colorPickerState.hsvaColor.s = s;
+            }
+        }, [snap.hsvaColor]
+    );
+
+    return (
+        <Saturation
+            className={classNames("w-56 h-52 border-foreground border", className)}
+            hue={snap.hsvaColor.h}
+            onChange={onSaturationValueChange}
+            {...rest}
+        >
+            <PointerCircle />
+        </Saturation>
+    );
+}
+
+function ColorDisplay() {
+    const snap = useSnapshot(colorPickerState);
+    const hexa = hsvaToHex(snap.hsvaColor);
+    return (
+        <div className="w-8 h-8 rounded-full" style={{ background: hexa }}></div>
+    );
+}
 
 function ColorNumbersDisplay() {
     const snap = useSnapshot(colorPickerState);
-    const hexa = snap.hsvaColor.a === 1 ? hsvaToHex(snap.hsvaColor): hsvaToHexa(snap.hsvaColor);
+    const hexa = snap.hsvaColor.a === 1 ? hsvaToHex(snap.hsvaColor) : hsvaToHexa(snap.hsvaColor);
     return (
         <div className="flex items-center space-x-2">
             <div className="w-4 h-4" style={{ background: hexa }}></div>
@@ -18,84 +99,19 @@ function ColorNumbersDisplay() {
     );
 }
 
-function AlphaView() {
-    const snap = useSnapshot(colorPickerState);
-    const { h, s, v, a } = snap.hsvaColor;
-
-    const onAlphaChange = useCallback(
-        (newAlpha: number) => {
-            if (colorPickerState.hsvaColor.a === newAlpha) {
-                return;
-            }
-            colorPickerState.hsvaColor.a = newAlpha;
-        }, [snap.hsvaColor]
-    );
-
-    return (
-        <Alpha
-            className="w-80 h-8 border-foreground border"
-            hsv={{ h, s, v }}
-            onChange={onAlphaChange}
-        >
-            <PointerCircleAlpha value={a * 100} />
-        </Alpha>
-    );
-}
-
-function HueView() {
-    const snap = useSnapshot(colorPickerState);
-    const { h } = snap.hsvaColor;
-
-    const onHueChange = useCallback(
-        (newHue: number) => {
-            if (colorPickerState.hsvaColor.h === newHue) {
-                return;
-            }
-            colorPickerState.hsvaColor.h = newHue;
-        }, [snap.hsvaColor]
-    );
-
-    return (
-        <Hue
-            className="w-80 h-8 border-foreground border"
-            hue={h}
-            onChange={onHueChange}
-        >
-            <PointerCircleAlpha value={(h / 360) * 100} />
-        </Hue>
-    );
-}
-
-function SaturationView() {
-    const snap = useSnapshot(colorPickerState);
-
-    const onSaturationValueChange = useCallback(
-        ({ s, v }: { s: number; v: number; }) => {
-            if (colorPickerState.hsvaColor.s === s && colorPickerState.hsvaColor.v === v) {
-                return;
-            }
-            colorPickerState.hsvaColor.v = v;
-            colorPickerState.hsvaColor.s = s;
-        }, [snap.hsvaColor]
-    );
-
-    return (
-        <Saturation
-            className="w-80 h-52 border-foreground border"
-            hue={snap.hsvaColor.h}
-            onChange={onSaturationValueChange}
-        >
-            <PointerCircle />
-        </Saturation>
-    );
-}
-
 export function SaturationSelector() {
     return (<>
-        <div className="grid gap-y-2">
-            <SaturationView />
-            <HueView />
-            <AlphaView />
+        <div className="p-2 inline-block bg-muted">
+            <div className="grid grid-cols-2 gap-y-1">
+                <SaturationView />
+
+                <ColorDisplay />
+
+                <div className="grid gap-y-1">
+                    <HueView />
+                    <AlphaView />
+                </div>
+            </div>
         </div>
 
         <ColorNumbersDisplay />
