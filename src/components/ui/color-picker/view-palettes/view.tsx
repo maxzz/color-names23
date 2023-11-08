088ec0ca27716ca384965c21c5f8a8a2b5ebdbd1
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-07 16:57:14 -0800

    WIP. color-picker. add palette menu prototype

--- src/components/ui/color-picker/view-palettes/view.tsx
+++ src/components/ui/color-picker/view-palettes/view.tsx
@@ -1,23 +1,26 @@
 import { HTMLAttributes } from "react";
 import { classNames } from "@/utils";
 import { MaterialPaletteShades, materialPalette } from "./material-palette";
+import { IconMenuBurger } from "../../icons";
+import { Button } from "../../shadcn";
 
 export function PaletteSelector({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className={classNames("flex space-x-2", className)} {...rest}>
-            
+        <div className={classNames("px-3 py-2 flex space-x-2", className)} {...rest}>
+
             <div className="flex-1 flex flex-wrap">
                 {materialPalette.colors.map((color, idx) => (
                     <div
-                        className={classNames("m-1 w-4 h-4 rounded")}
-                        // style={{ background: MaterialPaletteShades.get(color) || ''}}
-                        style={{ background: color}}
+                        className={classNames("m-1 w-3 h-3 rounded")}
+                        style={{ background: color }}
                         key={idx}
                     />
                 ))}
             </div>
 
-            <div className="flex-none">dropdown</div>
+            <Button variant={'outline'} size={'icon'} className="w-4 h-4 flex-none">
+                <IconMenuBurger className="w-4 h-4" />
+            </Button>
         </div>
     );
 }
