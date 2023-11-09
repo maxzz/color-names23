d0d4618c59d887e1f9b971882a73edf99b242774
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-08 18:26:52 -0800

    WIP. color-picker. ShadesPopup code clean up

--- src/components/ui/color-picker/view-palettes/view.tsx
+++ src/components/ui/color-picker/view-palettes/view.tsx
@@ -36,69 +36,12 @@ function ShadesPopup({ colorGroup, open, setOpen, className, ...rest }: { colorG
                                 }}
                             />
                         </div>
-
-                        {/* <div
-                            className={classNames("w-3 h-3", cellClasses)}
-                            style={{ background: color }}
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                e.preventDefault();
-                                onColorClick(color);
-                            }}
-                        /> */}
                     </Fragment>
                 ))}
             </PopoverContent>
         </Popover>
     );
 }
-
-// function ShadesPopup({ colorGroup, open, setOpen, className, ...rest }: { colorGroup: string; open: boolean; setOpen: (open: boolean) => void; className?: string; }) {
-//     const group = materialPalette.shades.get(colorGroup)!;
-//     function onColorClick(color: string) {
-//         colorPickerState.hsvaColor = hexToHsva(color);
-//         setOpen(false);
-//     }
-//     return (
-//         <Popover open={open} onOpenChange={setOpen}>
-//             <PopoverAnchor className="w-4 h-4" />
-//             <PopoverContent className={classNames("p-1 w-auto text-[.65rem] flex flex-col space-y-2 border-muted-foreground border !duration-ani-300", className)} {...rest}>
-//                 {group.map((color, idx) => (
-//                     <Fragment key={idx}>
-//                         {idx === materialPalette.extraIdx && (
-//                             <div className="-mx-1 h-px bg-muted-foreground/50" />
-//                         )}
-
-//                         <div className="">
-//                             <div className="">
-//                                 {materialPalette.shadeNames[idx]}
-//                             </div>
-//                             <div
-//                                 className={classNames("w-3 h-3", cellClasses)}
-//                                 style={{ background: color }}
-//                                 onClick={(e) => {
-//                                     e.stopPropagation();
-//                                     e.preventDefault();
-//                                     onColorClick(color);
-//                                 }}
-//                             />
-//                         </div>
-
-//                         {/* <div
-//                             className={classNames("w-3 h-3", cellClasses)}
-//                             style={{ background: color }}
-//                             onClick={(e) => {
-//                                 e.stopPropagation();
-//                                 e.preventDefault();
-//                                 onColorClick(color);
-//                             }}
-//                         /> */}
-//                     </Fragment>
-//                 ))}
-//             </PopoverContent>
-//         </Popover>
-//     );
-// }
 
 const cellClasses = "w-4 h-4 rounded transition-opacity delay-100";
 
