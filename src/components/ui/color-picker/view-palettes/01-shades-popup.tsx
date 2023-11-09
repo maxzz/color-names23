7b111f7edf08ca2e5316f6623d0f8cc39253d5f6
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-09 15:35:37 -0800

    WIP. color-picker. add palette and color names to popup

--- src/components/ui/color-picker/view-palettes/01-shades-popup.tsx
+++ src/components/ui/color-picker/view-palettes/01-shades-popup.tsx
@@ -15,18 +15,32 @@ type ShadesPopupProps = {
 
 export const cellClasses = "w-4 h-4 rounded transition-opacity delay-100";
 export const contentClasses = "\
+p-0 \
+w-auto \
+!duration-ani-300 \
+select-none \
+ring-0 \
+border-0 \
+bg-transparent \
+shadow-none \
+grid grid-cols-[auto,auto]";
+
+export const popupClasses = "\
 p-1 \
 w-auto \
 text-[.65rem] \
 space-y-2 \
 border-muted-foreground \
 border \
+rounded \
+bg-background \
+shadow \
 !duration-ani-300 \
 select-none \
 grid grid-cols-[auto,auto] gap-x-1";
 
 export function ShadesPopup({ colorGroup, open, setOpen, className, ...rest }: ShadesPopupProps) {
-    const {activePaletteIdx} = useSnapshot(palettePickerState);
+    const { activePaletteIdx } = useSnapshot(palettePickerState);
     const palette = paletteList[activePaletteIdx];
 
     const group = palette.shades.get(colorGroup)!;
@@ -40,29 +54,87 @@ export function ShadesPopup({ colorGroup, open, setOpen, className, ...rest }: S
         <Popover open={open} onOpenChange={setOpen}>
             <PopoverAnchor className="w-4 h-4" />
             <PopoverContent className={classNames(contentClasses, className)} align="end" alignOffset={-4} {...rest}>
-                {group.map((color, idx) => (
-                    <Fragment key={idx}>
-                        {idx === palette.extraIdx && (
-                            <div className="col-span-2 -mx-1 h-px bg-muted-foreground/50" />
-                        )}
+                <div className="py-1 text-sm font-semibold [writing-mode:vertical-rl] rotate-180">{palette.title}:{' '}{colorGroup}</div>
+                <div className={`${popupClasses}`}>
+                    {group.map((color, idx) => (
+                        <Fragment key={idx}>
+                            {idx === palette.extraIdx && (
+                                <div className="col-span-2 -mx-1 h-px bg-muted-foreground/50" />
+                            )}
 
-                        <div className="col-span-2 grid [grid-template-columns:subgrid]">
-                            <div className="text-right">
-                                {palette.shadeNames[idx]}
+                            <div className="col-span-2 grid [grid-template-columns:subgrid]">
+                                <div className="text-right">
+                                    {palette.shadeNames[idx]}
+                                </div>
+                                <div
+                                    className={classNames("w-3 h-3 cursor-pointer active:scale-95", cellClasses)}
+                                    style={{ background: color }}
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        e.preventDefault();
+                                        onColorClick(color);
+                                    }}
+                                />
                             </div>
-                            <div
-                                className={classNames("w-3 h-3 cursor-pointer active:scale-95", cellClasses)}
-                                style={{ background: color }}
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    e.preventDefault();
-                                    onColorClick(color);
-                                }}
-                            />
-                        </div>
-                    </Fragment>
-                ))}
+                        </Fragment>
+                    ))}
+
+                </div>
             </PopoverContent>
         </Popover>
     );
 }
+
+// export const cellClasses = "w-4 h-4 rounded transition-opacity delay-100";
+// export const contentClasses = "\
+// p-1 \
+// w-auto \
+// text-[.65rem] \
+// space-y-2 \
+// border-muted-foreground \
+// border \
+// !duration-ani-300 \
+// select-none \
+// grid grid-cols-[auto,auto] gap-x-1";
+
+// export function ShadesPopup({ colorGroup, open, setOpen, className, ...rest }: ShadesPopupProps) {
+//     const {activePaletteIdx} = useSnapshot(palettePickerState);
+//     const palette = paletteList[activePaletteIdx];
+
+//     const group = palette.shades.get(colorGroup)!;
+
+//     function onColorClick(color: string) {
+//         colorPickerState.hsvaColor = hexToHsva(color);
+//         setOpen(false);
+//     }
+
+//     return (
+//         <Popover open={open} onOpenChange={setOpen}>
+//             <PopoverAnchor className="w-4 h-4" />
+//             <PopoverContent className={classNames(contentClasses, className)} align="end" alignOffset={-4} {...rest}>
+//                 {group.map((color, idx) => (
+//                     <Fragment key={idx}>
+//                         {idx === palette.extraIdx && (
+//                             <div className="col-span-2 -mx-1 h-px bg-muted-foreground/50" />
+//                         )}
+
+//                         <div className="col-span-2 grid [grid-template-columns:subgrid]">
+//                             <div className="text-right">
+//                                 {palette.shadeNames[idx]}
+//                             </div>
+//                             <div
+//                                 className={classNames("w-3 h-3 cursor-pointer active:scale-95", cellClasses)}
+//                                 style={{ background: color }}
+//                                 onClick={(e) => {
+//                                     e.stopPropagation();
+//                                     e.preventDefault();
+//                                     onColorClick(color);
+//                                 }}
+//                             />
+//                         </div>
+//                     </Fragment>
+//                 ))}
+//             </PopoverContent>
+//         </Popover>
+//     );
+// }
