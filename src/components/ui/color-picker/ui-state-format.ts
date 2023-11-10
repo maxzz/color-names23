f25a0eb4dab995ea24efc9400be77c0ccbc5e5a6
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-09 18:02:48 -0800

    WIP. color-picker. format menu. add single row view

---
+++ src/components/ui/color-picker/ui-state-format.ts
@@ -0,0 +1,21 @@
+import { proxy } from "valtio";
+import { HsvaColor } from "./color-convert";
+
+export type FormatItem = {
+    name: string;
+    format: "hex" | "rgb" | "hsl";
+}
+
+export const formatList: FormatItem[] = [
+    { name: "Hex", format: "hex" },
+    { name: "RGB", format: "rgb" },
+    { name: "HSL", format: "hsl" },
+];
+
+export type FormatPickerState = {
+    formatIdx: number; // index of the selected format
+}
+
+export const formatPickerState = proxy<FormatPickerState>({
+    formatIdx: 0,
+});
