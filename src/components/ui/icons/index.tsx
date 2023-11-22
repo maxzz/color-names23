6d962d53f9b23cb5eacb5dca5113a862ce345ed9
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-21 17:20:59 -0800

    WIP. ViewColorOverBackground. add simplified copy icon

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -4,6 +4,7 @@ export * from './normal/01-app-logo';
 export * from './normal/02-checkbox';
 export * from './normal/02-chevrons';
 export * from './normal/02-clipboard';
+export * from './normal/02-copy';
 export * from './normal/02-menu-burger';
 export * from './normal/03-lock-locked';
 export * from './normal/03-lock-unlocked';
