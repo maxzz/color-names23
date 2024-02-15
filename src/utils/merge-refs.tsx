751d4b0daeb5a1c9e13601e4ad94776c8f79b30e
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-15 14:45:16 -0800

    WIP. add full default palette

--- src/utils/merge-refs.tsx
+++ src/utils/merge-refs.tsx
@@ -1,12 +1,12 @@
-import type * as React from "react";
+import type { LegacyRef, MutableRefObject, RefCallback } from "react";
 
-export function mergeRefs<T = any>(refs: Array<React.MutableRefObject<T> | React.LegacyRef<T> | undefined | null>): React.RefCallback<T> {
+export function mergeRefs<T = any>(refs: Array<MutableRefObject<T> | LegacyRef<T> | undefined | null>): RefCallback<T> {
     return (value) => {
         refs.forEach((ref) => {
             if (typeof ref === "function") {
                 ref(value);
             } else if (ref != null) {
-                (ref as React.MutableRefObject<T | null>).current = value;
+                (ref as MutableRefObject<T | null>).current = value;
             }
         });
     };
