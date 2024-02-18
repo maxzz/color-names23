ee642014ac10e245306363b9842157aceda88095
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-17 16:50:09 -0800

    WIP. shadcn. add scroll area

--- src/components/app/2-main/4-shadcn/01-top/0-top-panel.tsx
+++ src/components/app/2-main/4-shadcn/01-top/0-top-panel.tsx
@@ -9,12 +9,12 @@ function PasteInput() {
     const snap = useSnapshot(parseText, { sync: true });
     return (
         <Textarea
-            value={snap.text}
-            onChange={(e) => parseText.text = e.target.value}
             className="self-stretch min-h-[36px] text-xs smallscroll resizer [&::-webkit-resizer]:rounded [&::-webkit-resizer]:[backgroundSize:80%_80%]"
             rows={1}
             placeholder="Paste theme vars here"
             spellCheck={false}
+            value={snap.text}
+            onChange={(e) => parseText.text = e.target.value}
         />
     );
 }
@@ -28,13 +28,12 @@ export function TopPanel() {
             </Button>
 
             {/* test styles */}
-
             <div className="">
                 <div className="text-[.55rem]">theme styles</div>
                 <PasteInput />
             </div>
-            {/* test 1 */}
 
+            {/* test 1 */}
             <div className="flex flex-col items-center">
                 <div className="text-[.55rem] text-center">without context</div>
                 <div className="w-min flex flex-col space-y-1">
@@ -43,7 +42,6 @@ export function TopPanel() {
             </div>
 
             {/* test 2 */}
-
             <div className="flex flex-col items-center">
                 <div className="text-[.55rem] text-center">with context</div>
                 <div className="w-min flex flex-col space-y-1">
