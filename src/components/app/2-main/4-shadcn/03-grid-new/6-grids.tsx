c0488626ac05c1ee93b9390306bba749c8a825e6
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-19 13:33:35 -0800

    WIP. shadcn. tab access by theme ID

--- src/components/app/2-main/4-shadcn/03-grid-new/6-grids.tsx
+++ src/components/app/2-main/4-shadcn/03-grid-new/6-grids.tsx
@@ -36,13 +36,6 @@ export function TableInTabs() {
 export function ThemeGrids2() {
     const { length } = useSnapshot(shadcnAll.themes);
     return (<>
-        <div className="container mx-auto max-w-xl grid grid-cols-[min-content,minmax(0,12rem),minmax(0,12rem)] place-content-center gap-y-2">
-            {Array(length).fill(0).map((_, idx) => (
-                <GroupGrid idx={idx} key={shadcnAll.themes[idx].themeId} />
-            ))}
-        </div>
-
         <TableInTabs />
-    </>
-    );
+    </>);
 }
