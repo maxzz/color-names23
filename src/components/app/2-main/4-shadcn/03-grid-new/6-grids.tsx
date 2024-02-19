76aacb6112e460deacac794f47109d325eed91ba
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-18 18:55:10 -0800

    WIP. shadcn. grid. add tabs

--- src/components/app/2-main/4-shadcn/03-grid-new/6-grids.tsx
+++ src/components/app/2-main/4-shadcn/03-grid-new/6-grids.tsx
@@ -1,14 +1,39 @@
 import { useSnapshot } from "valtio";
 import { shadcnAll } from "@/store";
 import { GroupGrid } from "./5-group-grid";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@radix-ui/react-tabs";
+
+export function TableInTabs() {
+    return (
+        <Tabs defaultValue="table1">
+            <TabsList>
+                <TabsTrigger value="table1">Simple table</TabsTrigger>
+                <TabsTrigger value="table2">Data table</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="table1">
+                {/* <SimpleTableDemo className="m-auto max-w-sm" /> */}
+                1
+            </TabsContent>
+
+            <TabsContent value="table2">
+                {/* <DataTableDemo /> */}
+                2
+            </TabsContent>
+        </Tabs>
+    );
+}
+
 
 export function ThemeGrids2() {
     const { length } = useSnapshot(shadcnAll.themes);
-    return (
+    return (<>
         <div className="container mx-auto max-w-xl grid grid-cols-[min-content,minmax(0,12rem),minmax(0,12rem)] place-content-center gap-y-2">
             {Array(length).fill(0).map((_, idx) => (
                 <GroupGrid idx={idx} key={shadcnAll.themes[idx].themeId} />
             ))}
         </div>
+        <TableInTabs />
+        </>
     );
 }
