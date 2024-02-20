487f0f7d4ca32f1af9a8f76ebc357178f9423ede
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-19 16:23:26 -0800

    WIP. shadcn. fix current tab selection when number of modes changed

--- src/components/app/2-main/4-shadcn/03-grid-new/6-grids.tsx
+++ src/components/app/2-main/4-shadcn/03-grid-new/6-grids.tsx
@@ -2,37 +2,39 @@ import { useSnapshot } from "valtio";
 import { shadcnAll } from "@/store";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/shadcn";
 import { GroupGrid } from "./5-group-grid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+function tabIdStr(idx: number): string {
+    return `${idx}`;
+}
 
 function TableTabList() {
     const themes = useSnapshot(shadcnAll.themes);
     return (<>
         <TabsList>
             {themes.map((theme, idx) => (
-                <TabsTrigger key={theme.themeId} value={`tab${idx}`}>{theme.name}</TabsTrigger>
+                <TabsTrigger key={theme.themeId} value={tabIdStr(idx)}>{theme.name}</TabsTrigger>
             ))}
         </TabsList>
     </>);
 }
 
 function TableInTabs() {
-    const [currentSubTab, setCurrentSubTab] = useState(`tab${0}`);
     const { length } = useSnapshot(shadcnAll.themes);
+    const [currentSubTab, setCurrentSubTab] = useState(0);
+    useEffect(() => setCurrentSubTab((current) => current < length ? current : 0), [length]);
+
     if (!length) {
         return null;
     }
+
     return (
-        <Tabs className="container max-w-xl" value={currentSubTab}
-            onValueChange={(v) => {
-                console.log('changed', v);
-                setCurrentSubTab(v);
-            }}
-        >
+        <Tabs className="container max-w-xl" value={tabIdStr(currentSubTab)} onValueChange={(v) => setCurrentSubTab(+v)}>
             <TableTabList />
 
             {Array(length).fill(0).map((_, idx) => (
 
-                <TabsContent value={`tab${idx}`} key={shadcnAll.themes[idx].themeId}>
+                <TabsContent value={tabIdStr(idx)} key={shadcnAll.themes[idx].themeId}>
                     <div className="p-4 bg-muted rounded shadow">
                         <div className="grid grid-cols-[min-content,minmax(0,12rem),minmax(0,12rem)] place-content-center gap-y-2">
                             <GroupGrid idx={idx} />
