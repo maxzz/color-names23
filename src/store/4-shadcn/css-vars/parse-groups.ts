6bbb0f2c42659ca4a413eb24817f66048dc1984e
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-06 18:44:04 -0700

    WIP. shadcn colors.parseToGroups. nested values failed. 12. final code review

--- src/store/4-shadcn/css-vars/parse-groups.ts
+++ src/store/4-shadcn/css-vars/parse-groups.ts
@@ -29,78 +29,53 @@ const reVarName = /^--([^-]+)(?:-(.*))?/;
  * 
  * @param fileThemes 
  * ```json
- * {
- *     ":root": {
- *         "--background": "0 0% 100%",
- *         "--foreground": "222.2 47.4% 11.2%",
- * 
- *         "--warning": "49 100% 97%",
- *         "--warning-foreground": "31 92% 45%",
- *         "--warning-border": "49 91% 91%",
- * 
- *         "--mani-destructive": "359 100% 94%",
- *         "--mani-destructive-border": "359 100% 94%",
- *         "--mani-destructive-border3": "359 100% 94%",
- *     },
- *     ".dark": {
- *         "--background": "224 71% 4%",
- *         "--foreground": "213 31% 91%",
- *     }
- * }
+ {
+     ":root": {
+         "--background": "0 0% 100%",
+         "--foreground": "222.2 47.4% 11.2%",
+ 
+         "--warning": "49 100% 97%",
+         "--warning-foreground": "31 92% 45%",
+         "--warning-border": "49 91% 91%",
+ 
+         "--mani-destructive": "359 100% 94%",
+         "--mani-destructive-border": "359 100% 94%",
+         "--mani-destructive-border3": "359 100% 94%",
+     },
+     ".dark": {
+         "--background": "224 71% 4%",
+         "--foreground": "213 31% 91%",
+     }
+ }
  * ```
- * @returns 
- * now:
+ * @returns
  * ```json
- * {
- *     ":root": {
- *         "background": {DEFAULT: "0 0% 100%"},
- *         "foreground": {DEFAULT: "222.2 47.4% 11.2%"},
- *
- *         "warning": {
- *              "DEFAULT": "49 100% 97%",
- *              "foreground": "31 92% 45%",
- *              "border": "49 91% 91%",
- *         },
- *        "mani": {
- *             "destructive": "359 100% 94%",
- *             "destructive-border": "359 100% 94%",
- *             "destructive-border3": "359 100% 94%",
- *       },
- *     },
- *     ".dark": {
- *         "background": {DEFAULT: "224 71% 4%"},
- *         "foreground": {DEFAULT: "213 31% 91%"},
- *     }
- * }
- * ```
- * should be:
- * ```json
- * {
- *     ":root": {
- *         "background": "0 0% 100%",
- *         "foreground": "222.2 47.4% 11.2%",
- *
- *         "warning": {
- *              "DEFAULT": "49 100% 97%",
- *              "foreground": "31 92% 45%",
- *              "border": "49 91% 91%",
- *         },
- *         "mani": {
- *              "destructive": {
- *                  "DEFAULT": "359 100% 94%",
- *                  "border": "359 100% 94%",
- *                  "border3": "359 100% 94%",
- *            },
- *         },
- *     },
- *     ".dark": {
- *         "background": "224 71% 4%",
- *         "foreground": "213 31% 91%",
- *     }
- * }
+ {
+     ":root": {
+         "background": "0 0% 100%",
+         "foreground": "222.2 47.4% 11.2%",
+
+         "warning": {
+              "DEFAULT": "49 100% 97%",
+              "foreground": "31 92% 45%",
+              "border": "49 91% 91%",
+         },
+         "mani": {
+              "destructive": {
+                  "DEFAULT": "359 100% 94%",
+                  "border": "359 100% 94%",
+                  "border3": "359 100% 94%",
+            },
+         },
+     },
+     ".dark": {
+         "background": "224 71% 4%",
+         "foreground": "213 31% 91%",
+     }
+ }
  * ```
  */
-export function parseToGroups(fileThemes: FileThemes) {
+export function parseToGroups(fileThemes: FileThemes): Record<string, RecursiveKeyValuePair<string, string>> {
     const groups: Record<string, RecursiveKeyValuePair> = {}; // themeName -> ValueVars
 
     Object.entries(fileThemes).forEach(([themeName, theme]) => {
